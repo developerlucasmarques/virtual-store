5310939c5a7ba2f59543091b7004ed1fcfe84ff6
mlucasdev <lucasmsantos819@gmail.com>
2023-08-23 19:23:27 -0300

    refactor: change type param AddUserData to UserData

--- src/domain/usecases/add-user.ts
+++ src/domain/usecases/add-user.ts
@@ -1,12 +1,7 @@
 import type { EmailInUseError } from '../../interactions/errors/email-in-use-error'
 import type { Either } from '../../shared/either'
+import type { UserData } from '../entities/user'
 import type { InvalidEmailError, InvalidNameError, InvalidPasswordError } from '../entities/user/errors'
-
-export interface AddUserData {
-  name: string
-  email: string
-  password: string
-}
 
 export interface AccessToken {
   accesToken: string
@@ -15,5 +10,5 @@ export interface AccessToken {
 export type AddUserResponse = Either<InvalidNameError | InvalidEmailError | InvalidPasswordError | EmailInUseError, AccessToken>
 
 export interface AddUser {
-  perform: (data: AddUserData) => Promise<AddUserResponse>
+  perform: (data: UserData) => Promise<AddUserResponse>
 }

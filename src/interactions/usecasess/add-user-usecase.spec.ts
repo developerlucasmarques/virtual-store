3c9d3fe60df5c91b87f4042e9c39196c945ce039
mlucasdev <lucasmsantos819@gmail.com>
2023-08-23 19:43:43 -0300

    refactor: move sut creation to a factory method

--- src/interactions/usecasess/add-user-usecase.spec.ts
+++ src/interactions/usecasess/add-user-usecase.spec.ts
@@ -1,19 +1,26 @@
-import { User } from '../../domain/entities/user'
+import { User, type UserData } from '../../domain/entities/user'
 import { AddUserUseCase } from './add-user-usecase'
+
+const makeUserData = (): UserData => ({
+  name: 'any name',
+  email: 'any_email@mail.com',
+  password: 'abcd1234'
+})
+
+interface SutTypes {
+  sut: AddUserUseCase
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AddUserUseCase()
+  return { sut }
+}
 
 describe('AddUser UseCase', () => {
   it('Should call User Entity with correct values', async () => {
-    const sut = new AddUserUseCase()
+    const { sut } = makeSut()
     const createSpy = jest.spyOn(User, 'create')
-    await sut.perform({
-      name: 'any name',
-      email: 'any_email@mail.com',
-      password: 'abcd1234'
-    })
-    expect(createSpy).toHaveBeenCalledWith({
-      name: 'any name',
-      email: 'any_email@mail.com',
-      password: 'abcd1234'
-    })
+    await sut.perform(makeUserData())
+    expect(createSpy).toHaveBeenCalledWith(makeUserData())
   })
 })

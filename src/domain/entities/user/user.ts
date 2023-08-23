bd87481e5390dd34b9984dd1f73b74ce33208f8d
mlucasdev <lucasmsantos819@gmail.com>
2023-08-23 18:25:59 -0300

    feat: ensure User Entity return InvalidPasswordError if create Password fails

--- src/domain/entities/user/user.ts
+++ src/domain/entities/user/user.ts
@@ -1,14 +1,14 @@
 import { type Either, left, right } from '../../../shared/either'
-import type { InvalidNameError } from './errors'
+import type { InvalidEmailError, InvalidNameError, InvalidPasswordError } from './errors'
 import type { UserData } from './user-data'
-import { Email, Name } from './value-objects'
+import { Email, Name, Password } from './value-objects'
 
 export class User {
   private constructor (
     private readonly name: string
   ) {}
 
-  static create (data: UserData): Either<InvalidNameError, User> {
+  static create (data: UserData): Either<InvalidNameError | InvalidEmailError | InvalidPasswordError, User> {
     const name = Name.create(data.name)
     if (name.isLeft()) {
       return left(name.value)
@@ -16,6 +16,10 @@ export class User {
     const email = Email.create(data.email)
     if (email.isLeft()) {
       return left(email.value)
+    }
+    const password = Password.create(data.password)
+    if (password.isLeft()) {
+      return left(password.value)
     }
     return right(new User(data.name))
   }

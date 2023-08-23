75897181c07d535ea0d6939b353185564be37c85
mlucasdev <lucasmsantos819@gmail.com>
2023-08-23 18:23:36 -0300

    test: ensure User Entity return InvalidEmailError if create Email fails

--- src/domain/entities/user/user.spec.ts
+++ src/domain/entities/user/user.spec.ts
@@ -1,8 +1,8 @@
 import { left } from '../../../shared/either'
-import { InvalidNameError } from './errors'
+import { InvalidEmailError, InvalidNameError } from './errors'
 import { User } from './user'
 import type { UserData } from './user-data'
-import { Name } from './value-objects'
+import { Email, Name } from './value-objects'
 
 const makeUserData = (): UserData => ({
   name: 'any name',
@@ -13,9 +13,17 @@ const makeUserData = (): UserData => ({
 describe('User Entity', () => {
   it('Should return InvalidNameError if create Name fails', () => {
     jest.spyOn(Name, 'create').mockReturnValueOnce(
-      left(new InvalidNameError('invalid_name'))
+      left(new InvalidNameError('any_name'))
     )
     const sut = User.create(makeUserData())
-    expect(sut.value).toEqual(new InvalidNameError('invalid_name'))
+    expect(sut.value).toEqual(new InvalidNameError('any_name'))
+  })
+
+  it('Should return InvalidEmailError if create Email fails', () => {
+    jest.spyOn(Email, 'create').mockReturnValueOnce(
+      left(new InvalidEmailError('any_email@mail.com'))
+    )
+    const sut = User.create(makeUserData())
+    expect(sut.value).toEqual(new InvalidEmailError('any_email@mail.com'))
   })
 })

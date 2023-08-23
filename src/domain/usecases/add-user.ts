89750449d05b31c8deb0e83318d838f36ad1997e
mlucasdev <lucasmsantos819@gmail.com>
2023-08-23 18:58:24 -0300

    feat: create contract AddUser

---
+++ src/domain/usecases/add-user.ts
@@ -0,0 +1,14 @@
+import type { EmailInUseError } from '../../interactions/errors/email-in-use-error'
+import type { Either } from '../../shared/either'
+import type { UserData } from '../entities/user'
+import type { InvalidEmailError, InvalidNameError, InvalidPasswordError } from '../entities/user/errors'
+
+export interface AccessToken {
+  accesToken: string
+}
+
+export type AddUserResponse = Either<InvalidNameError | InvalidEmailError | InvalidPasswordError | EmailInUseError, AccessToken>
+
+export interface AddUser {
+  perform: (data: UserData) => Promise<AddUserResponse>
+}

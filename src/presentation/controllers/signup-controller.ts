ba4821d5be937bd5607b1d3c11821685b3d26b23
mlucasdev <lucasmsantos819@gmail.com>
2023-08-24 16:10:51 -0300

    feat: ensure SignUpController call AddUser with correct values

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,10 +1,12 @@
+import type { AddUser } from '@/domain/usecases'
 import type { Validation, Controller } from '../contracts'
 import { badRequest } from '../helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 
 export class SignUpController implements Controller {
   constructor (
-    private readonly validationComposite: Validation
+    private readonly validationComposite: Validation,
+    private readonly addUser: AddUser
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -12,6 +14,8 @@ export class SignUpController implements Controller {
     if (validation.isLeft()) {
       return badRequest(validation.value)
     }
+    const { name, email, password } = httpRequest.body
+    await this.addUser.perform({ name, email, password })
     return await Promise.resolve({
       body: '',
       statusCode: 0

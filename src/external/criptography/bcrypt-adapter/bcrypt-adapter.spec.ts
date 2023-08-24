63d25c8a70a582e72536608624423bc48f969d8b
mlucasdev <lucasmsantos819@gmail.com>
2023-08-24 16:59:31 -0300

    test: ensure BcryptAdapter integration with bcrypt

---
+++ src/external/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -0,0 +1,42 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve('hash')
+  }
+}))
+
+const salt = 12
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  test('Should call hash with correct values', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.hashing('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a valid hash on hash success', async () => {
+    const sut = makeSut()
+    const hash = await sut.hashing('any_value')
+    expect(hash).toEqual({ hash: 'hash' })
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash') as unknown as jest.Mock<
+    ReturnType<(key: Error) => Promise<Error>>,
+    Parameters<(key: Error) => Promise<Error>>
+    >
+    hashSpy.mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const promise = sut.hashing('any_value')
+    await expect(promise).rejects.toThrow()
+  })
+})

718b72dd9e67595e283e4a09b21f6ad5b021044c
mlucasdev <lucasmsantos819@gmail.com>
2023-08-24 16:23:21 -0300

    feat: ensure SignUpController return 500 if ValidationComposite throws

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,6 +1,6 @@
 import type { AddUser } from '@/domain/usecases'
 import type { Controller, Validation } from '../contracts'
-import { badRequest } from '../helpers/http/http-helpers'
+import { badRequest, serverError } from '../helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 
 export class SignUpController implements Controller {
@@ -10,18 +10,23 @@ export class SignUpController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validation = this.validationComposite.validate(httpRequest.body)
-    if (validation.isLeft()) {
-      return badRequest(validation.value)
+    try {
+      const validation = this.validationComposite.validate(httpRequest.body)
+      if (validation.isLeft()) {
+        return badRequest(validation.value)
+      }
+      const { name, email, password } = httpRequest.body
+      const addUserResult = await this.addUser.perform({ name, email, password })
+      if (addUserResult.isLeft()) {
+        return badRequest(addUserResult.value)
+      }
+      return await Promise.resolve({
+        body: '',
+        statusCode: 0
+      })
+    } catch (error: any) {
+      console.error(error)
+      return serverError(error)
     }
-    const { name, email, password } = httpRequest.body
-    const addUserResult = await this.addUser.perform({ name, email, password })
-    if (addUserResult.isLeft()) {
-      return badRequest(addUserResult.value)
-    }
-    return await Promise.resolve({
-      body: '',
-      statusCode: 0
-    })
   }
 }
--- src/presentation/helpers/http/http-helpers.ts
+++ src/presentation/helpers/http/http-helpers.ts
@@ -1,6 +1,12 @@
+import { ServerError } from '@/presentation/errors/server-error'
 import type { HttpResponse } from '@/presentation/http-types/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
 })

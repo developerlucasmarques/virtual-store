7e6392f08ebc321dfcc546ed40cd83fbe511d2ed
mlucasdev <lucasmsantos819@gmail.com>
2023-08-25 22:04:48 -0300

    feat: add Express AdaptRoute

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,21 @@
+import type { Controller } from '@/presentation/contracts'
+import type { HttpRequest } from '@/presentation/http-types/http'
+import type { Request, Response } from 'express'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } else {
+      res.status(httpResponse.statusCode).json({
+        name: httpResponse.body.name,
+        error: httpResponse.body.message,
+        statusCode: httpResponse.statusCode
+      })
+    }
+  }
+}

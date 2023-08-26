10e62d5c46a68b3de807cda24f1bfed67ea18872
mlucasdev <lucasmsantos819@gmail.com>
2023-08-25 21:47:40 -0300

    feat: add env configs

---
+++ src/main/config/env.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export default {
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/virtual-store',
+  port: process.env.PORT ?? 5050,
+  jwtSecretKey: process.env.JWT_SECRET_KEY ?? 'secret_key_Ls9a8çF0b4c3e5T6g7h2X1Qw3E4r5T6y7'
+}

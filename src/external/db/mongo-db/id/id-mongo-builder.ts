541d0a57cc36e5b5b33172d35f519c72c5f2b33e
mlucasdev <lucasmsantos819@gmail.com>
2023-08-25 21:47:19 -0300

    feat: ensure IdMongoBuilder return valid id if build on success

--- src/external/db/mongo-db/id/id-mongo-builder.ts
+++ src/external/db/mongo-db/id/id-mongo-builder.ts
@@ -1,5 +1,6 @@
 import type { Id, IdBuilder } from '@/interactions/contracts'
 import { ObjectId } from 'mongodb'
+
 export class IdMongoBuilder implements IdBuilder {
   build (): Id {
     const id = new ObjectId().toHexString()

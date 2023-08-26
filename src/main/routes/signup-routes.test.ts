d1acf7f64550b3b02b611ab06c4d3ceafc35395c
mlucasdev <lucasmsantos819@gmail.com>
2023-08-25 22:06:34 -0300

    test: ensure route api/signup return 201 on signup

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,33 @@
+import request from 'supertest'
+import app from '../config/app'
+import { type Collection } from 'mongodb'
+import { MongoHelper } from '@/external/db/mongo-db/helpers/mongo-helper'
+
+let userCollection: Collection
+
+describe('Access Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    userCollection = await MongoHelper.getCollection('user')
+    await userCollection.deleteMany({})
+  })
+
+  test('Should return 201 on signup', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'any name',
+        email: 'any_email@mail.com',
+        password: 'abcd1234',
+        passwordConfirmation: 'abcd1234'
+      })
+      .expect(201)
+  })
+})

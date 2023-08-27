83549fc071df7b7c9781159682db9901222adb3f
mlucasdev <lucasmsantos819@gmail.com>
2023-08-27 17:30:53 -0300

    feat: ensure LoginController return 500 if Auth throws

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,6 +1,6 @@
 import type { Auth } from '@/domain/usecases-contracts'
 import type { Controller, Validation } from '../contracts'
-import { badRequest, unauthorized } from '../helpers/http/http-helpers'
+import { badRequest, serverError, unauthorized } from '../helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 import { InvalidEmailError } from '@/domain/entities/user/errors'
 
@@ -11,18 +11,23 @@ export class LoginController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validations = this.validationComposite.validate(httpRequest.body)
-    if (validations.isLeft()) {
-      return badRequest(validations.value)
-    }
-    const { email, password } = httpRequest.body
-    const authResult = await this.auth.perform({ email, password })
-    if (authResult.isLeft()) {
-      if (authResult.value instanceof InvalidEmailError) {
-        return badRequest(authResult.value)
+    try {
+      const validations = this.validationComposite.validate(httpRequest.body)
+      if (validations.isLeft()) {
+        return badRequest(validations.value)
       }
-      return unauthorized(authResult.value)
+      const { email, password } = httpRequest.body
+      const authResult = await this.auth.perform({ email, password })
+      if (authResult.isLeft()) {
+        if (authResult.value instanceof InvalidEmailError) {
+          return badRequest(authResult.value)
+        }
+        return unauthorized(authResult.value)
+      }
+      return { statusCode: 0, body: '' }
+    } catch (error: any) {
+      console.error(error)
+      return serverError(error)
     }
-    return { statusCode: 0, body: '' }
   }
 }

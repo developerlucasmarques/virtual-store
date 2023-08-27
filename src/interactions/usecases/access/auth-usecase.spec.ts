b6c59d9d58e9b9f8b868d8ff8ca6ccefa002410d
mlucasdev <lucasmsantos819@gmail.com>
2023-08-27 15:38:26 -0300

    refactor: move sut creation to a factory method

--- src/interactions/usecases/access/auth-usecase.spec.ts
+++ src/interactions/usecases/access/auth-usecase.spec.ts
@@ -2,27 +2,38 @@ import { User } from '@/domain/entities/user'
 import { AuthUseCase } from './auth-usecase'
 import { left } from '@/shared/either'
 import { InvalidEmailError } from '@/domain/entities/user/errors'
+import type { AuthData } from '@/domain/usecases-contracts'
+
+const makeFakeAuthData = (): AuthData => ({
+  email: 'any_email@mail.com',
+  password: 'abcd1234'
+})
+
+type SutTypes = {
+  sut: AuthUseCase
+}
+
+const makeFakeSut = (): SutTypes => {
+  const sut = new AuthUseCase()
+  return {
+    sut
+  }
+}
 
 describe('Auth UseCase', () => {
   it('Should call User Entity validateEmail with correct email', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeFakeSut()
     const validateEmailSpy = jest.spyOn(User, 'validateEmail')
-    await sut.perform({
-      email: 'any_email@mail.com',
-      password: 'abcd1234'
-    })
+    await sut.perform(makeFakeAuthData())
     expect(validateEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 
   it('Should return InvalidEmailError if User Entity validateEmail returs InvalidEmailError', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeFakeSut()
     jest.spyOn(User, 'validateEmail').mockReturnValueOnce(
       left(new InvalidEmailError('any_email@mail.com'))
     )
-    const result = await sut.perform({
-      email: 'any_email@mail.com',
-      password: 'abcd1234'
-    })
+    const result = await sut.perform(makeFakeAuthData())
     expect(result.value).toEqual(new InvalidEmailError('any_email@mail.com'))
   })
 })

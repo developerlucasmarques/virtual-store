82db40eaf7000ab798a3260d75dcdf9a90cd3189
mlucasdev <lucasmsantos819@gmail.com>
2023-08-27 15:44:00 -0300

    feat: ensure AuthUseCase call LoadUserByEmailRepo with correct email

--- src/interactions/usecases/access/auth-usecase.ts
+++ src/interactions/usecases/access/auth-usecase.ts
@@ -1,13 +1,17 @@
 import { User } from '@/domain/entities/user'
 import type { Auth, AuthData, AuthResponse } from '@/domain/usecases-contracts'
+import type { LoadUserByEmailRepo } from '@/interactions/contracts'
 import { left, right } from '@/shared/either'
 
 export class AuthUseCase implements Auth {
+  constructor (private readonly loadUserByEmailRepo: LoadUserByEmailRepo) {}
+
   async perform (data: AuthData): Promise<AuthResponse> {
     const emailOrError = User.validateEmail(data.email)
     if (emailOrError.isLeft()) {
       return left(emailOrError.value)
     }
+    await this.loadUserByEmailRepo.loadByEmail(data.email)
     return right({ accessToken: 'any' })
   }
 }

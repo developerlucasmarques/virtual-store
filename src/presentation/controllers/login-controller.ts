fad3bacdd232f0960f77568877feef22e82b3316
mlucasdev <lucasmsantos819@gmail.com>
2023-08-27 17:19:54 -0300

    feat: ensure LoginController call Auth with correct values

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,15 +1,21 @@
+import type { Auth } from '@/domain/usecases-contracts'
 import type { Controller, Validation } from '../contracts'
 import { badRequest } from '../helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 
 export class LoginController implements Controller {
-  constructor (private readonly validationComposite: Validation) {}
+  constructor (
+    private readonly validationComposite: Validation,
+    private readonly auth: Auth
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const validations = this.validationComposite.validate(httpRequest.body)
     if (validations.isLeft()) {
       return badRequest(validations.value)
     }
+    const { email, password } = httpRequest.body
+    await this.auth.perform({ email, password })
     return { statusCode: 0, body: '' }
   }
 }

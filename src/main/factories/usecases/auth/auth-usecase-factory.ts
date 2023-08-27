2520e414bff12c53e179da9f7576ba6a29a678e1
mlucasdev <lucasmsantos819@gmail.com>
2023-08-27 17:40:27 -0300

    feat: create AuthUseCase factory

---
+++ src/main/factories/usecases/auth/auth-usecase-factory.ts
@@ -0,0 +1,12 @@
+import type { Auth } from '@/domain/usecases-contracts'
+import { BcryptAdapter } from '@/external/criptography/bcrypt-adapter/bcrypt-adapter'
+import { UserMongoRepo } from '@/external/db/mongo-db/user/user-mongo-repo'
+import { AuthUseCase } from '@/interactions/usecases/access/auth-usecase'
+import { makeAccessTokenBuilderUseCase } from '../access-token-builder/access-token-builder-factory'
+
+export const makeAuthUseCase = (): Auth => {
+  const userMongoRepo = new UserMongoRepo()
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  return new AuthUseCase(userMongoRepo, bcryptAdapter, makeAccessTokenBuilderUseCase())
+}

d842a73d5159b3c8a045028c3256214eac947ea4
mlucasdev <lucasmsantos819@gmail.com>
2023-08-27 14:11:52 -0300

    test: ensure User Entity return InvalidEmailError if validateEmail fails

--- src/domain/entities/user/user.spec.ts
+++ src/domain/entities/user/user.spec.ts
@@ -10,36 +10,48 @@ const makeUserData = (): UserData => ({
 })
 
 describe('User Entity', () => {
-  it('Should return InvalidNameError if create Name fails', () => {
-    jest.spyOn(Name, 'create').mockReturnValueOnce(
-      left(new InvalidNameError('any_name'))
-    )
-    const sut = User.create(makeUserData())
-    expect(sut.value).toEqual(new InvalidNameError('any_name'))
+  describe('create()', () => {
+    it('Should return InvalidNameError if create Name fails', () => {
+      jest.spyOn(Name, 'create').mockReturnValueOnce(
+        left(new InvalidNameError('any_name'))
+      )
+      const sut = User.create(makeUserData())
+      expect(sut.value).toEqual(new InvalidNameError('any_name'))
+    })
+
+    it('Should return InvalidEmailError if create Email fails', () => {
+      jest.spyOn(Email, 'create').mockReturnValueOnce(
+        left(new InvalidEmailError('any_email@mail.com'))
+      )
+      const sut = User.create(makeUserData())
+      expect(sut.value).toEqual(new InvalidEmailError('any_email@mail.com'))
+    })
+
+    it('Should return InvalidPasswordError if create Password fails', () => {
+      jest.spyOn(Password, 'create').mockReturnValueOnce(
+        left(new InvalidPasswordError('abcd1234'))
+      )
+      const sut = User.create(makeUserData())
+      expect(sut.value).toEqual(new InvalidPasswordError('abcd1234'))
+    })
+
+    it('Should create User if all data is valid', () => {
+      const sut = User.create(makeUserData())
+      expect(sut.value).toEqual({
+        name: Name.create('any name').value,
+        email: Email.create('any_email@mail.com').value,
+        password: Password.create('abcd1234').value
+      })
+    })
   })
 
-  it('Should return InvalidEmailError if create Email fails', () => {
-    jest.spyOn(Email, 'create').mockReturnValueOnce(
-      left(new InvalidEmailError('any_email@mail.com'))
-    )
-    const sut = User.create(makeUserData())
-    expect(sut.value).toEqual(new InvalidEmailError('any_email@mail.com'))
-  })
-
-  it('Should return InvalidPasswordError if create Password fails', () => {
-    jest.spyOn(Password, 'create').mockReturnValueOnce(
-      left(new InvalidPasswordError('abcd1234'))
-    )
-    const sut = User.create(makeUserData())
-    expect(sut.value).toEqual(new InvalidPasswordError('abcd1234'))
-  })
-
-  it('Should create User if all data is valid', () => {
-    const sut = User.create(makeUserData())
-    expect(sut.value).toEqual({
-      name: Name.create('any name').value,
-      email: Email.create('any_email@mail.com').value,
-      password: Password.create('abcd1234').value
+  describe('validateEmail()', () => {
+    it('Should return InvalidEmailError if create Email fails', () => {
+      jest.spyOn(Email, 'create').mockReturnValueOnce(
+        left(new InvalidEmailError('any_email@mail.com'))
+      )
+      const sut = User.validateEmail('any_email@mail.com')
+      expect(sut.value).toEqual(new InvalidEmailError('any_email@mail.com'))
     })
   })
 })

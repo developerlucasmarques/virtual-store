e26072579e53c45b9561da8f6e90c1268a2d577f
mlucasdev <lucasmsantos819@gmail.com>
2023-08-27 15:29:22 -0300

    test: ensure AuthUseCase return InvalidEmailError if User Entity validateEmail returns InvalidEmailError

--- src/interactions/usecases/access/auth-usecase.spec.ts
+++ src/interactions/usecases/access/auth-usecase.spec.ts
@@ -1,5 +1,7 @@
 import { User } from '@/domain/entities/user'
 import { AuthUseCase } from './auth-usecase'
+import { left } from '@/shared/either'
+import { InvalidEmailError } from '@/domain/entities/user/errors'
 
 describe('Auth UseCase', () => {
   it('Should call User Entity validateEmail with correct email', async () => {
@@ -10,5 +12,17 @@ describe('Auth UseCase', () => {
       password: 'abcd1234'
     })
     expect(validateEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  it('Should return InvalidEmailError if User Entity validateEmail returs InvalidEmailError', async () => {
+    const sut = new AuthUseCase()
+    jest.spyOn(User, 'validateEmail').mockReturnValueOnce(
+      left(new InvalidEmailError('any_email@mail.com'))
+    )
+    const result = await sut.perform({
+      email: 'any_email@mail.com',
+      password: 'abcd1234'
+    })
+    expect(result.value).toEqual(new InvalidEmailError('any_email@mail.com'))
   })
 })

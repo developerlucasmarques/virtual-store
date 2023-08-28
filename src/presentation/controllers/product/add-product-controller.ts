79c6459afa0ecc747138d2e5d36933deea691be5
mlucasdev <lucasmsantos819@gmail.com>
2023-08-28 18:12:50 -0300

    feat: ensure AddProductController return 500 if ValidationComposite throws

--- src/presentation/controllers/product/add-product-controller.ts
+++ src/presentation/controllers/product/add-product-controller.ts
@@ -1,15 +1,19 @@
 import type { Controller, Validation } from '@/presentation/contracts'
-import { badRequest } from '@/presentation/helpers/http/http-helpers'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class AddProductController implements Controller {
   constructor (private readonly validationComposite: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validations = this.validationComposite.validate(httpRequest.body)
-    if (validations.isLeft()) {
-      return badRequest(validations.value)
+    try {
+      const validations = this.validationComposite.validate(httpRequest.body)
+      if (validations.isLeft()) {
+        return badRequest(validations.value)
+      }
+      return { statusCode: 0, body: '' }
+    } catch (error: any) {
+      return serverError(error)
     }
-    return { statusCode: 0, body: '' }
   }
 }

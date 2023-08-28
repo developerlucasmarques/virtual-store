0623337712e1b7a5f81754c7078e10e9289c29b3
mlucasdev <lucasmsantos819@gmail.com>
2023-08-28 16:37:00 -0300

    test: ensure AddProductUseCase return a Error if create Product fails

--- src/interactions/usecases/product/add-product-usecase.spec.ts
+++ src/interactions/usecases/product/add-product-usecase.spec.ts
@@ -1,5 +1,6 @@
 import { Product, type ProductData } from '@/domain/entities/product'
 import { AddProductUseCase } from './add-product-usecase'
+import { left } from '@/shared/either'
 
 const makeFakeProductData = (): ProductData => ({
   name: 'any name',
@@ -23,5 +24,14 @@ describe('AddProduct UseCase', () => {
     const createSpy = jest.spyOn(Product, 'create')
     await sut.perform(makeFakeProductData())
     expect(createSpy).toHaveBeenCalledWith(makeFakeProductData())
+  })
+
+  it('Should return a Error if create Product fails', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(Product, 'create').mockReturnValueOnce(
+      left(new Error('any message'))
+    )
+    const result = await sut.perform(makeFakeProductData())
+    expect(result.value).toEqual(new Error('any message'))
   })
 })

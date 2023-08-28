55411be945e841715a7ffe83d66e0d964d8d6543
mlucasdev <lucasmsantos819@gmail.com>
2023-08-28 17:01:10 -0300

    feat: ensure AddProductUseCase call IdBuilder

--- src/interactions/usecases/product/add-product-usecase.ts
+++ src/interactions/usecases/product/add-product-usecase.ts
@@ -1,13 +1,17 @@
 import { Product, type ProductData } from '@/domain/entities/product'
-import type { AddProduct, AddProductResponse } from '@/domain/usecases-contracts/product/add-product'
+import type { AddProduct, AddProductResponse } from '@/domain/usecases-contracts'
+import type { IdBuilder } from '@/interactions/contracts'
 import { left, right } from '@/shared/either'
 
 export class AddProductUseCase implements AddProduct {
+  constructor (private readonly idBuilder: IdBuilder) {}
+
   async perform (data: ProductData): Promise<AddProductResponse> {
     const product = Product.create(data)
     if (product.isLeft()) {
       return left(product.value)
     }
+    this.idBuilder.build()
     return right(null)
   }
 }

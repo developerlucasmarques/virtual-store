f577715957b70955a14b5ca86487d874fc8e491a
mlucasdev <lucasmsantos819@gmail.com>
2023-08-28 11:45:45 -0300

    test: ensure Product return InvalidProductAmountError if create ProductAmount fails

--- src/domain/entities/product/product.spec.ts
+++ src/domain/entities/product/product.spec.ts
@@ -1,6 +1,6 @@
 import { left } from '@/shared/either'
-import { ProductName } from './value-objects'
-import { InvalidProductNameError } from './errors'
+import { ProductAmount, ProductName } from './value-objects'
+import { InvalidProductAmountError, InvalidProductNameError } from './errors'
 import { Product, type ProductData } from './product'
 
 const makeFakeProductData = (): ProductData => ({
@@ -10,11 +10,19 @@ const makeFakeProductData = (): ProductData => ({
 })
 
 describe('Product Entity', () => {
-  it('Should return InvalidProductName if create ProductName fails', () => {
+  it('Should return InvalidProductNameError if create ProductName fails', () => {
     jest.spyOn(ProductName, 'create').mockReturnValueOnce(
       left(new InvalidProductNameError('any_name'))
     )
     const sut = Product.create(makeFakeProductData())
     expect(sut.value).toEqual(new InvalidProductNameError('any_name'))
+  })
+
+  it('Should return InvalidProductAmountError if create ProductAmount fails', () => {
+    jest.spyOn(ProductAmount, 'create').mockReturnValueOnce(
+      left(new InvalidProductAmountError(10))
+    )
+    const sut = Product.create(makeFakeProductData())
+    expect(sut.value).toEqual(new InvalidProductAmountError(10))
   })
 })

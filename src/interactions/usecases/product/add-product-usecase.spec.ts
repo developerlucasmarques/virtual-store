772919aaed8cc2467199931f1561bb026a8d3e40
mlucasdev <lucasmsantos819@gmail.com>
2023-08-28 16:34:18 -0300

    refactor: move sut creation to a factory method

--- src/interactions/usecases/product/add-product-usecase.spec.ts
+++ src/interactions/usecases/product/add-product-usecase.spec.ts
@@ -1,21 +1,27 @@
-import { Product } from '@/domain/entities/product'
+import { Product, type ProductData } from '@/domain/entities/product'
 import { AddProductUseCase } from './add-product-usecase'
+
+const makeFakeProductData = (): ProductData => ({
+  name: 'any name',
+  amount: 10.90,
+  description: 'any description',
+  image: 'any image'
+})
+
+type SutTypes = {
+  sut: AddProductUseCase
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AddProductUseCase()
+  return { sut }
+}
 
 describe('AddProduct UseCase', () => {
   it('Should call Product Entity with correct values', async () => {
-    const sut = new AddProductUseCase()
+    const { sut } = makeSut()
     const createSpy = jest.spyOn(Product, 'create')
-    await sut.perform({
-      name: 'any name',
-      amount: 10.90,
-      description: 'any description',
-      image: 'any image'
-    })
-    expect(createSpy).toHaveBeenCalledWith({
-      name: 'any name',
-      amount: 10.90,
-      description: 'any description',
-      image: 'any image'
-    })
+    await sut.perform(makeFakeProductData())
+    expect(createSpy).toHaveBeenCalledWith(makeFakeProductData())
   })
 })

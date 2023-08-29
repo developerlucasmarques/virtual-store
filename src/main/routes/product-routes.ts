6064760b89a153ebb15e04163b926669c1e4f334
mlucasdev <lucasmsantos819@gmail.com>
2023-08-29 15:01:24 -0300

    feat: ensure post in /product route return 401 on add product without x-access-token

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -1,7 +1,9 @@
 import type { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddProductController } from '../factories/controllers/product/add-product-factory'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAccessControlMiddleware } from '../factories/middleware/access-control-middleware-factory'
 
 export default async (router: Router): Promise<void> => {
-  router.post('/product', adaptRoute(makeAddProductController()))
+  router.post('/product', adaptMiddleware(makeAccessControlMiddleware('admin')), adaptRoute(makeAddProductController()))
 }

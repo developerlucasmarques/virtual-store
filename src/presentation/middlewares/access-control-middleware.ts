3caa48467584e68a99415e3fca7e5bc730a21a7a
mlucasdev <lucasmsantos819@gmail.com>
2023-08-29 14:40:35 -0300

    feat: ensure AccessControlMiddleware return 500 if AccessControl throws

--- src/presentation/middlewares/access-control-middleware.ts
+++ src/presentation/middlewares/access-control-middleware.ts
@@ -1,7 +1,7 @@
 import type { AccessControl } from '@/domain/usecases-contracts'
 import type { Middleware } from '../contracts/middleware'
 import { AccessTokenNotInformedError } from '../errors'
-import { forbidden, unauthorized } from '../helpers/http/http-helpers'
+import { forbidden, serverError, unauthorized } from '../helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 import type { RoleModel } from '@/domain/models'
 import { InvalidTokenError } from '@/domain/usecases-contracts/export-errors'
@@ -13,19 +13,23 @@ export class AccessControlMiddleware implements Middleware {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    if (!accessToken) {
-      return unauthorized(new AccessTokenNotInformedError())
-    }
-    const accessControleResult = await this.accessControl.perform({
-      accessToken, role: this.role
-    })
-    if (accessControleResult.isLeft()) {
-      if (accessControleResult.value instanceof InvalidTokenError) {
-        return unauthorized(accessControleResult.value)
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (!accessToken) {
+        return unauthorized(new AccessTokenNotInformedError())
       }
-      return forbidden(accessControleResult.value)
+      const accessControleResult = await this.accessControl.perform({
+        accessToken, role: this.role
+      })
+      if (accessControleResult.isLeft()) {
+        if (accessControleResult.value instanceof InvalidTokenError) {
+          return unauthorized(accessControleResult.value)
+        }
+        return forbidden(accessControleResult.value)
+      }
+      return { statusCode: 0, body: '' }
+    } catch (error: any) {
+      return serverError(error)
     }
-    return { statusCode: 0, body: '' }
   }
 }

00fa8300f2ce61856f02338d2c2e66b52435734c
mlucasdev <lucasmsantos819@gmail.com>
2023-08-29 16:49:50 -0300

    feat: ensure LoadAllProductsController return 204 if LoadAllProducts returns empty list

--- src/presentation/controllers/product/load-all-products-controller.ts
+++ src/presentation/controllers/product/load-all-products-controller.ts
@@ -1,13 +1,14 @@
 import type { LoadAllProducts } from '@/domain/usecases-contracts'
 import type { Controller } from '@/presentation/contracts'
-import { serverError } from '@/presentation/helpers/http/http-helpers'
+import { noContent, serverError } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadAllProductsController implements Controller {
   constructor (private readonly loadAllProducts: LoadAllProducts) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadAllProducts.perform()
+      const products = await this.loadAllProducts.perform()
+      if (products.length === 0) return noContent()
       return { statusCode: 0, body: '' }
     } catch (error: any) {
       console.error(error)

4ede6361d5b768023c1413b6c816566da9cf95c6
mlucasdev <lucasmsantos819@gmail.com>
2023-08-29 14:30:44 -0300

    test: enaure AccessControlMiddleware call AccessControl with correct values

--- src/presentation/middlewares/access-control-middleware.spec.ts
+++ src/presentation/middlewares/access-control-middleware.spec.ts
@@ -1,11 +1,53 @@
+import type { AccessControl, AccessControlData, AccessControlResponse } from '@/domain/usecases-contracts'
 import { AccessTokenNotInformedError } from '../errors'
 import { unauthorized } from '../helpers/http/http-helpers'
 import { AccessControlMiddleware } from './access-control-middleware'
+import { right } from '@/shared/either'
+import type { HttpRequest } from '../http-types/http'
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: { 'x-access-token': 'any_token' }
+})
+
+const makeFakeAccessControlData = (): AccessControlData => ({
+  accessToken: 'any_token',
+  role: 'admin'
+})
+
+const makeAccessControlStub = (): AccessControl => {
+  class AccessControlStub implements AccessControl {
+    async perform (data: AccessControlData): Promise<AccessControlResponse> {
+      return await Promise.resolve(right({ userId: 'any_id' }))
+    }
+  }
+  return new AccessControlStub()
+}
+
+type SutTypes = {
+  sut: AccessControlMiddleware
+  accessControlStub: AccessControl
+}
+
+const makeSut = (): SutTypes => {
+  const accessControlStub = makeAccessControlStub()
+  const sut = new AccessControlMiddleware(accessControlStub, 'admin')
+  return {
+    sut,
+    accessControlStub
+  }
+}
 
 describe('AccessControl Middleware', () => {
   it('Should return 401 if x-access-token not provided in headers', async () => {
-    const sut = new AccessControlMiddleware()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(unauthorized(new AccessTokenNotInformedError()))
+  })
+
+  it('Should call AccessControl with correct values', async () => {
+    const { sut, accessControlStub } = makeSut()
+    const performSpy = jest.spyOn(accessControlStub, 'perform')
+    await sut.handle(makeFakeRequest())
+    expect(performSpy).toHaveBeenCalledWith(makeFakeAccessControlData())
   })
 })

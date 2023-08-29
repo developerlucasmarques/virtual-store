aaa6d122058c3185be16e05054897b8b264e1e22
mlucasdev <lucasmsantos819@gmail.com>
2023-08-29 16:32:01 -0300

    feat: ensure ProductMongoRepo load all products on success

--- src/external/db/mongo-db/product/product-mongo-repo.ts
+++ src/external/db/mongo-db/product/product-mongo-repo.ts
@@ -1,11 +1,18 @@
 import type { ProductModel } from '@/domain/models'
-import type { AddProductRepo } from '@/interactions/contracts'
+import type { AddProductRepo, LoadAllProductsRepo } from '@/interactions/contracts'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class ProductMongoRepo implements AddProductRepo {
+export class ProductMongoRepo implements AddProductRepo, LoadAllProductsRepo {
   async add (data: ProductModel): Promise<void> {
     const product = MongoHelper.convertCollectionIdStringToObjectId(data)
     const productCollection = await MongoHelper.getCollection('product')
     await productCollection.insertOne(product)
+  }
+
+  async loadAll (): Promise<ProductModel[]> {
+    const productCollection = await MongoHelper.getCollection('product')
+    const products = await productCollection.find().toArray()
+    const productsFormated = products.map((product) => (MongoHelper.convertCollectionIdObjectIdToString(product)))
+    return productsFormated
   }
 }

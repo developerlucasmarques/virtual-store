8ae5411e36f98a8d51cc046f98294e810bd5bf36
mlucasdev <lucasmsantos819@gmail.com>
2023-08-29 18:13:46 -0300

    feat: ensure LoadProductByIdUseCase return ProductNotFoundError if LoadProductByIdRepo returns null

--- src/interactions/usecases/product/load-product-by-id/load-product-by-id-usecase.ts
+++ src/interactions/usecases/product/load-product-by-id/load-product-by-id-usecase.ts
@@ -1,12 +1,16 @@
 import type { LoadProductByIdResponse, LoadProductById } from '@/domain/usecases-contracts'
+import { ProductNotFoundError } from '@/domain/usecases-contracts/export-errors'
 import type { LoadProductByIdRepo } from '@/interactions/contracts'
-import { right } from '@/shared/either'
+import { left, right } from '@/shared/either'
 
 export class LoadProductByIdUseCase implements LoadProductById {
   constructor (private readonly loadProductByIdRepo: LoadProductByIdRepo) {}
 
   async perform (productId: string): Promise<LoadProductByIdResponse> {
-    await this.loadProductByIdRepo.loadById(productId)
+    const product = await this.loadProductByIdRepo.loadById(productId)
+    if (!product) {
+      return left(new ProductNotFoundError(productId))
+    }
     return right({ id: '', amount: 0, description: '', name: '' })
   }
 }

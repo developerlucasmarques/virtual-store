5cc03b7d7b9e103d1e8cb3812b4332a610a70667
mlucasdev <lucasmsantos819@gmail.com>
2023-08-29 16:47:42 -0300

    feat: ensure LoadAllProductsController return 500 if LoadAllProducts throws

--- src/presentation/controllers/product/load-all-products-controller.ts
+++ src/presentation/controllers/product/load-all-products-controller.ts
@@ -1,11 +1,17 @@
 import type { LoadAllProducts } from '@/domain/usecases-contracts'
 import type { Controller } from '@/presentation/contracts'
+import { serverError } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadAllProductsController implements Controller {
   constructor (private readonly loadAllProducts: LoadAllProducts) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadAllProducts.perform()
-    return { statusCode: 0, body: '' }
+    try {
+      await this.loadAllProducts.perform()
+      return { statusCode: 0, body: '' }
+    } catch (error: any) {
+      console.error(error)
+      return serverError(error)
+    }
   }
 }

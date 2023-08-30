239f4ccbcd01b6c7d89b304c9b9be2f1bdeeb2aa
mlucasdev <lucasmsantos819@gmail.com>
2023-08-30 17:35:20 -0300

    test: ensure RequiredParamValidation return InvalidParamsError if not informed required param

---
+++ src/presentation/helpers/validators/required-param-validation.spec.ts
@@ -0,0 +1,14 @@
+import { InvalidParamsError } from '@/presentation/errors/invalid-params-error'
+import { RequiredParamValidation } from './required-param-validation'
+
+const makeSut = (paramName: string): RequiredParamValidation => {
+  return new RequiredParamValidation(paramName)
+}
+
+describe('RequiredParam Validation', () => {
+  it('Should return InvalidParamsError if not informed required param', () => {
+    const sut = makeSut('userId')
+    const result = sut.validate({ paramId: 'any_id' })
+    expect(result.value).toEqual(new InvalidParamsError('userId'))
+  })
+})

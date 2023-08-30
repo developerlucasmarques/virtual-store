293c95a22aa0e35e1ed625bd71bdb7294f4e065d
mlucasdev <lucasmsantos819@gmail.com>
2023-08-30 17:06:54 -0300

    feat: ensure LoadProductByIdController return 400 if Validation fails

--- src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller.ts
+++ src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller.ts
@@ -1,11 +1,15 @@
 import type { Controller, Validation } from '@/presentation/contracts'
+import { badRequest } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadProductByIdController implements Controller {
-  constructor (private readonly validationComposite: Validation) {}
+  constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validationComposite.validate(httpRequest.params?.productId)
+    const validation = this.validation.validate(httpRequest.params?.productId)
+    if (validation.isLeft()) {
+      return badRequest(validation.value)
+    }
     return { statusCode: 0, body: '' }
   }
 }

88a83db8b7ec1414cc14465a1fd7e0eadc0fa663
mlucasdev <lucasmsantos819@gmail.com>
2023-08-30 17:40:19 -0300

    refactor: add export

--- src/presentation/controllers/product/index.ts
+++ src/presentation/controllers/product/index.ts
@@ -1,2 +1,3 @@
 export * from './add-product/add-product-controller'
 export * from './load-all-products/load-all-products-controller'
+export * from './load-product-by-id/load-product-by-id-controller'

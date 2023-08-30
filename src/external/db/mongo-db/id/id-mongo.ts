2bc90bc8e855ccad91d05af96462f64fa4116d2b
mlucasdev <lucasmsantos819@gmail.com>
2023-08-30 16:48:49 -0300

    feat: ensure IdMongo return true if isValid is a success

--- src/external/db/mongo-db/id/id-mongo.ts
+++ src/external/db/mongo-db/id/id-mongo.ts
@@ -1,9 +1,13 @@
-import type { Id, IdBuilder } from '@/interactions/contracts'
+import type { Id, IdBuilder, ValidationId } from '@/interactions/contracts'
 import { ObjectId } from 'mongodb'
 
-export class IdMongo implements IdBuilder {
+export class IdMongo implements IdBuilder, ValidationId {
   build (): Id {
     const id = new ObjectId().toHexString()
     return { id }
+  }
+
+  isValid (id: string): boolean {
+    return ObjectId.isValid(id)
   }
 }

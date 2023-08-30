810902c8bb890d062b367520d8267b2e954bb1eb
mlucasdev <lucasmsantos819@gmail.com>
2023-08-30 16:48:55 -0300

    test: ensure IdMongo return true if isValid is a success

--- src/external/db/mongo-db/id/id-mongo.spec.ts
+++ src/external/db/mongo-db/id/id-mongo.spec.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb'
 import { IdMongo } from './id-mongo'
 
 describe('IdMongoBuilder', () => {
@@ -6,5 +7,17 @@ describe('IdMongoBuilder', () => {
     const build = sut.build()
     const pattern = /^[0-9a-fA-F]{24}$/
     expect(pattern.test(build.id)).toBeTruthy()
+  })
+
+  it('Should return false if isValid fails', () => {
+    const sut = new IdMongo()
+    const isValid = sut.isValid('idString')
+    expect(isValid).toBe(false)
+  })
+
+  it('Should return true if isValid is a success', () => {
+    const sut = new IdMongo()
+    const isValid = sut.isValid(new ObjectId().toHexString())
+    expect(isValid).toBe(true)
   })
 })

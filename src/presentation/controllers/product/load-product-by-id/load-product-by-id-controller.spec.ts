6fd951b7c087edccc27df7e8146587ccce1daa82
mlucasdev <lucasmsantos819@gmail.com>
2023-08-30 17:07:13 -0300

    test: ensure LoadProductByIdController return 400 if Validation fails

--- src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller.spec.ts
+++ src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller.spec.ts
@@ -1,7 +1,8 @@
 import type { Validation } from '@/presentation/contracts'
 import type { HttpRequest } from '@/presentation/http-types/http'
-import { type Either, right } from '@/shared/either'
+import { type Either, right, left } from '@/shared/either'
 import { LoadProductByIdController } from './load-product-by-id-controller'
+import { badRequest } from '@/presentation/helpers/http/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -9,34 +10,43 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
-const makeValidationComposite = (): Validation => {
-  class ValidationCompositeStub implements Validation {
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
     validate (input: any): Either<Error, null> {
       return right(null)
     }
   }
-  return new ValidationCompositeStub()
+  return new ValidationStub()
 }
 
 type SutTypes = {
   sut: LoadProductByIdController
-  validationCompositeStub: Validation
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
-  const validationCompositeStub = makeValidationComposite()
-  const sut = new LoadProductByIdController(validationCompositeStub)
+  const validationStub = makeValidation()
+  const sut = new LoadProductByIdController(validationStub)
   return {
     sut,
-    validationCompositeStub
+    validationStub
   }
 }
 
 describe('LoadProductById Controller', () => {
-  it('Should call ValidationComposite with correct values', async () => {
-    const { sut, validationCompositeStub } = makeSut()
-    const validateSpy = jest.spyOn(validationCompositeStub, 'validate')
+  it('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(makeFakeRequest())
     expect(validateSpy).toHaveBeenLastCalledWith('any_id')
+  })
+
+  it('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      left(new Error('any_message'))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error('any_message')))
   })
 })

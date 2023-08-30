e3b3be5149560b3e9484746a0c4aeb063f4f6d6e
mlucasdev <lucasmsantos819@gmail.com>
2023-08-30 17:14:45 -0300

    feat: ensure LoadProductByIdController call LoadProductById with correct id

--- src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller.ts
+++ src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller.ts
@@ -1,15 +1,20 @@
+import type { LoadProductById } from '@/domain/usecases-contracts'
 import type { Controller, Validation } from '@/presentation/contracts'
 import { badRequest } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadProductByIdController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly loadProductById: LoadProductById
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validation = this.validation.validate(httpRequest.params?.productId)
+    const validation = this.validation.validate(httpRequest.params.productId)
     if (validation.isLeft()) {
       return badRequest(validation.value)
     }
+    await this.loadProductById.perform(httpRequest.params.productId)
     return { statusCode: 0, body: '' }
   }
 }

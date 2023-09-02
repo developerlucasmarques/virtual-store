3bf497702d2d7224e7a5757ef43162771a894c4b
mlucasdev <lucasmsantos819@gmail.com>
2023-09-01 22:16:36 -0300

    feat: ensure CreateCartUseCase call LoadProductByIdRepo with correct product id

--- src/interactions/usecases/cart/create-cart-usecase.ts
+++ src/interactions/usecases/cart/create-cart-usecase.ts
@@ -1,14 +1,16 @@
 import type { AddProductToCartData, CreateCart, CreateCartReponse } from '@/domain/usecases-contracts'
-import type { CreateCartRepo, IdBuilder } from '@/interactions/contracts'
+import type { CreateCartRepo, IdBuilder, LoadProductByIdRepo } from '@/interactions/contracts'
 import { right } from '@/shared/either'
 
 export class CreateCartUseCase implements CreateCart {
   constructor (
+    private readonly loadProductByIdRepo: LoadProductByIdRepo,
     private readonly idBuilder: IdBuilder,
     private readonly createCartRepo: CreateCartRepo
   ) {}
 
   async perform (data: AddProductToCartData): Promise<CreateCartReponse> {
+    await this.loadProductByIdRepo.loadById(data.productId)
     const { id } = this.idBuilder.build()
     await this.createCartRepo.create({ id, ...data })
     return right(null)

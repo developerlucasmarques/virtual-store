ec524f1a93d6576a5e335b68924cdfba8d439124
mlucasdev <lucasmsantos819@gmail.com>
2023-09-01 22:19:39 -0300

    feat: ensure CreateCartUseCase return ProductNotFoundError if LoadProductByIdRepo returns null

--- src/interactions/usecases/cart/create-cart-usecase.ts
+++ src/interactions/usecases/cart/create-cart-usecase.ts
@@ -1,6 +1,7 @@
 import type { AddProductToCartData, CreateCart, CreateCartReponse } from '@/domain/usecases-contracts'
+import { ProductNotFoundError } from '@/domain/usecases-contracts/errors'
 import type { CreateCartRepo, IdBuilder, LoadProductByIdRepo } from '@/interactions/contracts'
-import { right } from '@/shared/either'
+import { left, right } from '@/shared/either'
 
 export class CreateCartUseCase implements CreateCart {
   constructor (
@@ -10,7 +11,10 @@ export class CreateCartUseCase implements CreateCart {
   ) {}
 
   async perform (data: AddProductToCartData): Promise<CreateCartReponse> {
-    await this.loadProductByIdRepo.loadById(data.productId)
+    const product = await this.loadProductByIdRepo.loadById(data.productId)
+    if (!product) {
+      return left(new ProductNotFoundError(data.productId))
+    }
     const { id } = this.idBuilder.build()
     await this.createCartRepo.create({ id, ...data })
     return right(null)

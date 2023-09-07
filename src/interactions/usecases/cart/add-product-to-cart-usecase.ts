d69f391701a8bfe27c61c0e0e0fd0925a9ad8b03
mlucasdev <lucasmsantos819@gmail.com>
2023-09-07 20:17:09 -0300

    feat: ensure AddProductToCartUseCase increase the quantity of the product if the product is already in the cart

--- src/interactions/usecases/cart/add-product-to-cart-usecase.ts
+++ src/interactions/usecases/cart/add-product-to-cart-usecase.ts
@@ -13,7 +13,7 @@ export class AddProductToCartUseCase implements AddProductToCart {
   ) {}
 
   async perform (data: AddProductToCartData): Promise<AddProductToCartResponse> {
-    const { productId, productQty, userId } = data
+    const { productId, userId } = data
     if (data.productQty < 1) {
       return left(new InvalidProductQuantityError())
     }
@@ -25,12 +25,18 @@ export class AddProductToCartUseCase implements AddProductToCart {
     if (!cart) {
       const { id } = this.idBuilder.build()
       await this.createCartRepo.create({
-        id, userId, product: { id: productId, quantity: productQty }
+        id, userId, product: { id: productId, quantity: data.productQty }
       })
       return right(null)
     }
+    let quantity = data.productQty
+    for (const product of cart.products) {
+      if (productId === product.id) {
+        quantity += product.quantity
+      }
+    }
     await this.addProductToCartRepo.addProduct({
-      id: cart?.id, product: { id: productId, quantity: productQty }
+      id: cart?.id, product: { id: productId, quantity }
     })
     return right(null)
   }

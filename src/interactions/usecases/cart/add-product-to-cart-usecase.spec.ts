6ebfa300cde6b2564c1e057cf883a8af9177369a
mlucasdev <lucasmsantos819@gmail.com>
2023-09-07 17:58:26 -0300

    test: ensure AddProductToCartUseCase return ProductNotFoundError if LoadProductByIdRepo returns null

--- src/interactions/usecases/cart/add-product-to-cart-usecase.spec.ts
+++ src/interactions/usecases/cart/add-product-to-cart-usecase.spec.ts
@@ -1,6 +1,14 @@
 import type { ProductModel } from '@/domain/models'
 import type { LoadProductByIdRepo } from '@/interactions/contracts'
 import { AddProductToCartUseCase } from './add-product-to-cart-usecese'
+import type { AddProductToCartData } from '@/domain/usecases-contracts'
+import { ProductNotFoundError } from '@/domain/usecases-contracts/errors'
+
+const makeFakeAddProductToCartData = (): AddProductToCartData => ({
+  userId: 'any_user_id',
+  productId: 'any_product_id',
+  productQty: 2
+})
 
 const makeFakeProductModel = (): ProductModel => ({
   id: 'any_id',
@@ -36,11 +44,16 @@ describe('AddProductToCart UseCase', () => {
   it('Should call LoadProductByIdRepo with correct product id', async () => {
     const { sut, loadProductByIdRepoStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadProductByIdRepoStub, 'loadById')
-    await sut.perform({
-      userId: 'any_user_id',
-      productId: 'any_product_id',
-      productQty: 1
-    })
+    await sut.perform(makeFakeAddProductToCartData())
     expect(loadByIdSpy).toHaveBeenCalledWith('any_product_id')
+  })
+
+  it('Shoul return ProductNotFoundError if LoadProductByIdRepo returns null', async () => {
+    const { sut, loadProductByIdRepoStub } = makeSut()
+    jest.spyOn(loadProductByIdRepoStub, 'loadById').mockReturnValueOnce(
+      Promise.resolve(null)
+    )
+    const result = await sut.perform(makeFakeAddProductToCartData())
+    expect(result.value).toEqual(new ProductNotFoundError('any_product_id'))
   })
 })

96519fa93ffab2a21a49c824bf5c1a5bb5d63c74
mlucasdev <lucasmsantos819@gmail.com>
2023-09-09 16:52:43 -0300

    test: ensure CartMongoRepo create a Cart if create is a success

---
+++ src/external/db/mongo-db/cart/cart-mongo-repo.test.ts
@@ -0,0 +1,46 @@
+import type { CreateCartRepoData } from '@/interactions/contracts'
+import { ObjectId, type Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { CartMongoRepo } from './cart-mongo-repo'
+
+const idString = new ObjectId().toHexString()
+const objectId = new ObjectId(idString)
+
+const makeFakeCreateCartRepoData = (): CreateCartRepoData => ({
+  id: idString,
+  userId: 'any_user_id',
+  product: {
+    id: 'any_product_id',
+    quantity: 1
+  }
+})
+
+let cartCollection: Collection
+
+const makeSut = (): CartMongoRepo => {
+  const sut = new CartMongoRepo()
+  return sut
+}
+
+describe('CartMongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    cartCollection = await MongoHelper.getCollection('cart')
+    await cartCollection.deleteMany({})
+  })
+
+  it('Should create a Cart if create is a success', async () => {
+    const sut = makeSut()
+    await sut.create(makeFakeCreateCartRepoData())
+    const cart = await cartCollection.findOne({ _id: objectId })
+    const cartWithMongoId = MongoHelper.convertCollectionIdStringToObjectId(makeFakeCreateCartRepoData())
+    expect(cart).toEqual(cartWithMongoId)
+  })
+})

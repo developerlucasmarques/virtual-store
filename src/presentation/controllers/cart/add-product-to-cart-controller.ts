f9d1f465f7307435d5c4269bbeb7f0cf6a3e545d
mlucasdev <lucasmsantos819@gmail.com>
2023-09-09 15:48:49 -0300

    feat: ensure AddProductToCartController return 400 if ValidationComposite fails

--- src/presentation/controllers/cart/add-product-to-cart-controller.ts
+++ src/presentation/controllers/cart/add-product-to-cart-controller.ts
@@ -1,11 +1,15 @@
 import type { Controller, Validation } from '@/presentation/contracts'
+import { badRequest } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class AddProductToCartController implements Controller {
   constructor (private readonly validationComposite: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validationComposite.validate(httpRequest.body)
+    const validations = this.validationComposite.validate(httpRequest.body)
+    if (validations.isLeft()) {
+      return badRequest(validations.value)
+    }
     return { statusCode: 0, body: '' }
   }
 }

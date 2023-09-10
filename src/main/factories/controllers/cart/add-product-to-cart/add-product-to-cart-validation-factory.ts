f6a8e21277bf61b420520499ef2b07a30c3d23bd
mlucasdev <lucasmsantos819@gmail.com>
2023-09-10 18:17:11 -0300

    feat: add IdTypeValidation to AddProductToCartValidation

--- src/main/factories/controllers/cart/add-product-to-cart/add-product-to-cart-validation-factory.ts
+++ src/main/factories/controllers/cart/add-product-to-cart/add-product-to-cart-validation-factory.ts
@@ -1,5 +1,7 @@
+import { IdMongo } from '@/external/db/mongo-db/id/id-mongo'
 import type { Validation } from '@/presentation/contracts'
 import { OnlyRequiredFieldsValidation, PrimitiveTypeValidation, RequiredFieldValidation, ValidationComposite } from '@/presentation/helpers/validators'
+import { IdTypeValidation } from '@/presentation/helpers/validators/id-type-validation'
 
 export const makeAddProductToCartValidation = (): ValidationComposite => {
   const validations: Validation[] = []
@@ -7,10 +9,12 @@ export const makeAddProductToCartValidation = (): ValidationComposite => {
   for (const field of requiredFields) {
     validations.push(new RequiredFieldValidation(field))
   }
+  const idValidator = new IdMongo()
   validations.push(
     new PrimitiveTypeValidation('productId', 'string'),
     new PrimitiveTypeValidation('productQty', 'number'),
-    new OnlyRequiredFieldsValidation(requiredFields)
+    new OnlyRequiredFieldsValidation(requiredFields),
+    new IdTypeValidation('productId', idValidator)
   )
   return new ValidationComposite(validations)
 }

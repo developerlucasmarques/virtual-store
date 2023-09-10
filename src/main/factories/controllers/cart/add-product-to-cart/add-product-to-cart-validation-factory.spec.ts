8ab9c15ef2e5be7b93122919f21d5b47f2c710e1
mlucasdev <lucasmsantos819@gmail.com>
2023-09-10 18:17:16 -0300

    test: add IdTypeValidation to AddProductToCartValidation

--- src/main/factories/controllers/cart/add-product-to-cart/add-product-to-cart-validation-factory.spec.ts
+++ src/main/factories/controllers/cart/add-product-to-cart/add-product-to-cart-validation-factory.spec.ts
@@ -1,6 +1,8 @@
 import type { Validation } from '@/presentation/contracts'
 import { OnlyRequiredFieldsValidation, PrimitiveTypeValidation, RequiredFieldValidation, ValidationComposite } from '@/presentation/helpers/validators'
 import { makeAddProductToCartValidation } from './add-product-to-cart-validation-factory'
+import { IdTypeValidation } from '@/presentation/helpers/validators/id-type-validation'
+import { IdMongo } from '@/external/db/mongo-db/id/id-mongo'
 
 jest.mock('@/presentation/helpers/validators/validation-composite')
 
@@ -12,10 +14,12 @@ describe('AddProductToCartValidation Factory', () => {
     for (const field of requiredFields) {
       validations.push(new RequiredFieldValidation(field))
     }
+    const idValidator = new IdMongo()
     validations.push(
       new PrimitiveTypeValidation('productId', 'string'),
       new PrimitiveTypeValidation('productQty', 'number'),
-      new OnlyRequiredFieldsValidation(requiredFields)
+      new OnlyRequiredFieldsValidation(requiredFields),
+      new IdTypeValidation('productId', idValidator)
     )
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })

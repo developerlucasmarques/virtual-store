8b2341f1e83bf8f3f6c126fd2451a0738f0ab148
mlucasdev <lucasmsantos819@gmail.com>
2023-09-10 16:08:01 -0300

    feat: ensure CartMongoRepo update the quantity of a product in the cart if updateProductQty is a success

--- src/external/db/mongo-db/cart/cart-mongo-repo.ts
+++ src/external/db/mongo-db/cart/cart-mongo-repo.ts
@@ -1,8 +1,8 @@
-import type { AddProductToCartRepo, AddProductToCartRepoData, CreateCartRepo, CreateCartRepoData, LoadCartByUserIdRepo } from '@/interactions/contracts'
+import type { AddProductToCartRepo, AddProductToCartRepoData, CreateCartRepo, CreateCartRepoData, LoadCartByUserIdRepo, UpdateProductQtyCartRepo } from '@/interactions/contracts'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { type CartModel } from '@/domain/models'
 
-export class CartMongoRepo implements CreateCartRepo, AddProductToCartRepo, LoadCartByUserIdRepo {
+export class CartMongoRepo implements CreateCartRepo, AddProductToCartRepo, LoadCartByUserIdRepo, UpdateProductQtyCartRepo {
   async create (data: CreateCartRepoData): Promise<void> {
     const { id, product, userId } = data
     const cartWithProductArray = { id, userId, products: [product] }
@@ -21,5 +21,14 @@ export class CartMongoRepo implements CreateCartRepo, AddProductToCartRepo, Load
     const cartCollection = await MongoHelper.getCollection('cart')
     const cart = await cartCollection.findOne({ userId })
     return MongoHelper.convertCollectionIdObjectIdToString(cart)
+  }
+
+  async updateProductQty (data: AddProductToCartRepoData): Promise<void> {
+    const objectId = MongoHelper.transformIdInObjectId(data.id)
+    const cartCollection = await MongoHelper.getCollection('cart')
+    await cartCollection.updateOne(
+      { _id: objectId, 'products.id': data.product.id },
+      { $inc: { 'products.$.quantity': data.product.quantity } }
+    )
   }
 }

d175eb6cb1a69c260a0d7fa47bd14d0a00897693
mlucasdev <lucasmsantos819@gmail.com>
2023-09-10 15:53:48 -0300

    feat: ensure CartMongoRepo load cart if loadByUserId is a success

--- src/external/db/mongo-db/cart/cart-mongo-repo.ts
+++ src/external/db/mongo-db/cart/cart-mongo-repo.ts
@@ -1,7 +1,8 @@
-import type { AddProductToCartRepo, AddProductToCartRepoData, CreateCartRepo, CreateCartRepoData } from '@/interactions/contracts'
+import type { AddProductToCartRepo, AddProductToCartRepoData, CreateCartRepo, CreateCartRepoData, LoadCartByUserIdRepo } from '@/interactions/contracts'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { type CartModel } from '@/domain/models'
 
-export class CartMongoRepo implements CreateCartRepo, AddProductToCartRepo {
+export class CartMongoRepo implements CreateCartRepo, AddProductToCartRepo, LoadCartByUserIdRepo {
   async create (data: CreateCartRepoData): Promise<void> {
     const { id, product, userId } = data
     const cartWithProductArray = { id, userId, products: [product] }
@@ -14,5 +15,11 @@ export class CartMongoRepo implements CreateCartRepo, AddProductToCartRepo {
     const cartCollection = await MongoHelper.getCollection('cart')
     const objectId = MongoHelper.transformIdInObjectId(data.id)
     await cartCollection.updateOne({ _id: objectId }, { $push: { products: data.product } })
+  }
+
+  async loadByUserId (userId: string): Promise< null | CartModel> {
+    const cartCollection = await MongoHelper.getCollection('cart')
+    const cart = await cartCollection.findOne({ userId })
+    return MongoHelper.convertCollectionIdObjectIdToString(cart)
   }
 }

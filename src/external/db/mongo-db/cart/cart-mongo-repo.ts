59e7c22c6442367cdb5869b50ac3a201b7fa22c9
mlucasdev <lucasmsantos819@gmail.com>
2023-09-10 15:39:27 -0300

    fix: ensure that the product will be inside an array

--- src/external/db/mongo-db/cart/cart-mongo-repo.ts
+++ src/external/db/mongo-db/cart/cart-mongo-repo.ts
@@ -3,7 +3,9 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class CartMongoRepo implements CreateCartRepo, AddProductToCartRepo {
   async create (data: CreateCartRepoData): Promise<void> {
-    const cart = MongoHelper.convertCollectionIdStringToObjectId(data)
+    const { id, product, userId } = data
+    const cartWithProductArray = { id, userId, products: [product] }
+    const cart = MongoHelper.convertCollectionIdStringToObjectId(cartWithProductArray)
     const cartCollection = await MongoHelper.getCollection('cart')
     await cartCollection.insertOne(cart)
   }

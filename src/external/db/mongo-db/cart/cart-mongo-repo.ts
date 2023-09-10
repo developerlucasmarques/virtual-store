c0d8d6cd6fc0e8047515374da2415fe4c907b2c3
mlucasdev <lucasmsantos819@gmail.com>
2023-09-10 15:01:42 -0300

    feat: ensure CartMongoRepo add product to Cart if addProduct is a success

--- src/external/db/mongo-db/cart/cart-mongo-repo.ts
+++ src/external/db/mongo-db/cart/cart-mongo-repo.ts
@@ -1,10 +1,16 @@
-import type { CreateCartRepo, CreateCartRepoData } from '@/interactions/contracts'
+import type { AddProductToCartRepo, AddProductToCartRepoData, CreateCartRepo, CreateCartRepoData } from '@/interactions/contracts'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class CartMongoRepo implements CreateCartRepo {
+export class CartMongoRepo implements CreateCartRepo, AddProductToCartRepo {
   async create (data: CreateCartRepoData): Promise<void> {
     const cart = MongoHelper.convertCollectionIdStringToObjectId(data)
     const cartCollection = await MongoHelper.getCollection('cart')
     await cartCollection.insertOne(cart)
+  }
+
+  async addProduct (data: AddProductToCartRepoData): Promise<void> {
+    const cartCollection = await MongoHelper.getCollection('cart')
+    const objectId = MongoHelper.transformIdInObjectId(data.id)
+    await cartCollection.updateOne({ _id: objectId }, { $push: { products: data.product } })
   }
 }

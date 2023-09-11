97201fd24464169ff811c02f5822764caa39c703
mlucasdev <lucasmsantos819@gmail.com>
2023-09-11 14:02:53 -0300

    feat: ensure LoadCartUseCase call LoadProductsByIdsRepo with correct product ids

--- src/interactions/usecases/cart/load-cart/load-cart-usecase.ts
+++ src/interactions/usecases/cart/load-cart/load-cart-usecase.ts
@@ -1,16 +1,21 @@
 import type { LoadCart, LoadCartResponse } from '@/domain/usecases-contracts'
 import { EmptyCartError } from '@/domain/usecases-contracts/errors'
-import type { LoadCartByUserIdRepo } from '@/interactions/contracts'
+import type { LoadCartByUserIdRepo, LoadProductsByIdsRepo } from '@/interactions/contracts'
 import { left, right } from '@/shared/either'
 
 export class LoadCartUseCase implements LoadCart {
-  constructor (private readonly loadCartByUserIdRepo: LoadCartByUserIdRepo) {}
+  constructor (
+    private readonly loadCartByUserIdRepo: LoadCartByUserIdRepo,
+    private readonly loadProductsByIdsRepo: LoadProductsByIdsRepo
+  ) {}
 
   async perform (userId: string): Promise<LoadCartResponse> {
     const cart = await this.loadCartByUserIdRepo.loadByUserId(userId)
     if (!cart) {
       return left(new EmptyCartError())
     }
+    const productIds = cart.products.map((product) => (product.id))
+    await this.loadProductsByIdsRepo.loadProductsByIds(productIds)
     return right({
       id: '',
       userId: '',

89abe99b6cbe3b1445359c11759575bcbfca9625
mlucasdev <lucasmsantos819@gmail.com>
2023-09-12 10:39:41 -0300

    feat: ensure LoadCartController return 400 if LoadCart returns an error

--- src/presentation/controllers/cart/load-cart/load-cart-controller.ts
+++ src/presentation/controllers/cart/load-cart/load-cart-controller.ts
@@ -1,12 +1,16 @@
 import type { LoadCart } from '@/domain/usecases-contracts'
 import type { Controller } from '@/presentation/contracts'
+import { badRequest } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadCartController implements Controller {
   constructor (private readonly loadCart: LoadCart) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadCart.perform(httpRequest.headers.userId)
+    const loadCartResult = await this.loadCart.perform(httpRequest.headers.userId)
+    if (loadCartResult.isLeft()) {
+      return badRequest(loadCartResult.value)
+    }
     return { statusCode: 0, body: '' }
   }
 }

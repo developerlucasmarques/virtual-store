bea1fb301bc1fc3264ff6012b91c8778bf1ccdaf
mlucasdev <lucasmsantos819@gmail.com>
2023-09-12 10:42:40 -0300

    feat: ensure LoadCartController return 500 if LoadCart throws

--- src/presentation/controllers/cart/load-cart/load-cart-controller.ts
+++ src/presentation/controllers/cart/load-cart/load-cart-controller.ts
@@ -1,16 +1,20 @@
 import type { LoadCart } from '@/domain/usecases-contracts'
 import type { Controller } from '@/presentation/contracts'
-import { badRequest } from '@/presentation/helpers/http/http-helpers'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadCartController implements Controller {
   constructor (private readonly loadCart: LoadCart) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const loadCartResult = await this.loadCart.perform(httpRequest.headers.userId)
-    if (loadCartResult.isLeft()) {
-      return badRequest(loadCartResult.value)
+    try {
+      const loadCartResult = await this.loadCart.perform(httpRequest.headers.userId)
+      if (loadCartResult.isLeft()) {
+        return badRequest(loadCartResult.value)
+      }
+      return { statusCode: 0, body: '' }
+    } catch (error: any) {
+      return serverError(error)
     }
-    return { statusCode: 0, body: '' }
   }
 }

84cf24d01d364eb4679b053bcb0fa29b0192555f
mlucasdev <lucasmsantos819@gmail.com>
2023-09-12 13:02:46 -0300

    feat: ensure LoadCartController return 404 if LoadCart returns ProductNotAvailableError

--- src/presentation/controllers/cart/load-cart/load-cart-controller.ts
+++ src/presentation/controllers/cart/load-cart/load-cart-controller.ts
@@ -1,6 +1,6 @@
 import type { LoadCart } from '@/domain/usecases-contracts'
 import type { Controller } from '@/presentation/contracts'
-import { badRequest, ok, serverError } from '@/presentation/helpers/http/http-helpers'
+import { notFound, ok, serverError } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadCartController implements Controller {
@@ -10,7 +10,7 @@ export class LoadCartController implements Controller {
     try {
       const loadCartResult = await this.loadCart.perform(httpRequest.headers.userId)
       if (loadCartResult.isLeft()) {
-        return badRequest(loadCartResult.value)
+        return notFound(loadCartResult.value)
       }
       return ok(loadCartResult.value)
     } catch (error: any) {

f18f8046d21cc10affcb5419ef85a3a0e675145e
mlucasdev <lucasmsantos819@gmail.com>
2023-09-12 13:06:09 -0300

    feat: ensure LoadCartController return 204 if LoadCart returns EmptyCartError

--- src/presentation/controllers/cart/load-cart/load-cart-controller.ts
+++ src/presentation/controllers/cart/load-cart/load-cart-controller.ts
@@ -1,6 +1,7 @@
 import type { LoadCart } from '@/domain/usecases-contracts'
+import { EmptyCartError } from '@/domain/usecases-contracts/errors'
 import type { Controller } from '@/presentation/contracts'
-import { notFound, ok, serverError } from '@/presentation/helpers/http/http-helpers'
+import { noContent, notFound, ok, serverError } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadCartController implements Controller {
@@ -10,6 +11,9 @@ export class LoadCartController implements Controller {
     try {
       const loadCartResult = await this.loadCart.perform(httpRequest.headers.userId)
       if (loadCartResult.isLeft()) {
+        if (loadCartResult.value instanceof EmptyCartError) {
+          return noContent()
+        }
         return notFound(loadCartResult.value)
       }
       return ok(loadCartResult.value)

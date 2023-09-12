7bd471ced6801173e42180bfc2d33a3c3e0c676e
mlucasdev <lucasmsantos819@gmail.com>
2023-09-12 12:24:55 -0300

    refactor: add http method to jest describe block

--- src/main/routes/access-routes.test.ts
+++ src/main/routes/access-routes.test.ts
@@ -20,7 +20,7 @@ describe('Access Routes', () => {
     await userCollection.deleteMany({})
   })
 
-  describe('/signup', () => {
+  describe('POST /signup', () => {
     it('Should return 201 on signup', async () => {
       await request(app)
         .post('/api/signup')
@@ -34,7 +34,7 @@ describe('Access Routes', () => {
     })
   })
 
-  describe('/login', () => {
+  describe('POST /login', () => {
     it('Should return 200 on login', async () => {
       const encryptedPassword = await hash('abcd1234', 12)
       await userCollection.insertOne({

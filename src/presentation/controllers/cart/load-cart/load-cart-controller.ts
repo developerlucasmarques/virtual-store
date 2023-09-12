7665cdd9ee5e59bf95712d286a22dad17151d188
mlucasdev <lucasmsantos819@gmail.com>
2023-09-12 10:44:21 -0300

    feat: ensure LoadCartController return 200 if LoadCart is a success

--- src/presentation/controllers/cart/load-cart/load-cart-controller.ts
+++ src/presentation/controllers/cart/load-cart/load-cart-controller.ts
@@ -1,6 +1,6 @@
 import type { LoadCart } from '@/domain/usecases-contracts'
 import type { Controller } from '@/presentation/contracts'
-import { badRequest, serverError } from '@/presentation/helpers/http/http-helpers'
+import { badRequest, ok, serverError } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class LoadCartController implements Controller {
@@ -12,7 +12,7 @@ export class LoadCartController implements Controller {
       if (loadCartResult.isLeft()) {
         return badRequest(loadCartResult.value)
       }
-      return { statusCode: 0, body: '' }
+      return ok(loadCartResult.value)
     } catch (error: any) {
       return serverError(error)
     }

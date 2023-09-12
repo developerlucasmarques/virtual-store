e74cbdc0e2c320e4201eebd8dd2ca7685300e39e
mlucasdev <lucasmsantos819@gmail.com>
2023-09-12 10:59:03 -0300

    feat: ensure ProductMongoRepo return many products if loadProductsByIds is a success

--- src/external/db/mongo-db/product/product-mongo-repo.ts
+++ src/external/db/mongo-db/product/product-mongo-repo.ts
@@ -1,8 +1,8 @@
 import type { ProductModel } from '@/domain/models'
-import type { AddProductRepo, LoadAllProductsRepo, LoadProductByIdRepo } from '@/interactions/contracts'
+import type { AddProductRepo, LoadAllProductsRepo, LoadProductByIdRepo, LoadProductsByIdsRepo } from '@/interactions/contracts'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class ProductMongoRepo implements AddProductRepo, LoadAllProductsRepo, LoadProductByIdRepo {
+export class ProductMongoRepo implements AddProductRepo, LoadAllProductsRepo, LoadProductByIdRepo, LoadProductsByIdsRepo {
   async add (data: ProductModel): Promise<void> {
     const product = MongoHelper.convertCollectionIdStringToObjectId(data)
     const productCollection = await MongoHelper.getCollection('product')
@@ -21,5 +21,13 @@ export class ProductMongoRepo implements AddProductRepo, LoadAllProductsRepo, Lo
     const objectId = MongoHelper.transformIdInObjectId(id)
     const product = await productCollection.findOne({ _id: objectId })
     return MongoHelper.convertCollectionIdObjectIdToString(product)
+  }
+
+  async loadProductsByIds (productIds: string[]): Promise<ProductModel[]> {
+    const productCollection = await MongoHelper.getCollection('product')
+    const ids = productIds.map((id) => MongoHelper.transformIdInObjectId(id))
+    const products = await productCollection.find({ _id: { $in: ids } }).toArray()
+    const productsFormated = products.map((product) => (MongoHelper.convertCollectionIdObjectIdToString(product)))
+    return productsFormated
   }
 }

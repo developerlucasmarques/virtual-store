b3ca915f19ee130eb3e3aac09e8c307cc582bb4c
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 15:47:09 -0300

    test: ensure CheckoutController return 500 if Checkout throws

--- src/presentation/controllers/checkout/checkout-controller.spec.ts
+++ src/presentation/controllers/checkout/checkout-controller.spec.ts
@@ -79,4 +79,13 @@ describe('Checkout Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new CheckoutFailureError()))
   })
+
+  it('Should return 500 if Checkout throws', async () => {
+    const { sut, checkoutStub } = makeSut()
+    jest.spyOn(checkoutStub, 'perform').mockReturnValueOnce(
+      Promise.reject(new Error())
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })

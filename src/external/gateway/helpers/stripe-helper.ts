6b9bbb484fcb8b9450845bfff6554a81bf2229de
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 17:40:52 -0300

    feat: ensure StripeHelper reconnect if key not provided in connection

--- src/external/gateway/helpers/stripe-helper.ts
+++ src/external/gateway/helpers/stripe-helper.ts
@@ -1,19 +1,28 @@
 import Stripe from 'stripe'
+import env from '@/main/config/env'
 
 export const StripeHelper = {
   client: null as unknown as Stripe | null,
 
   async connect (key: string | undefined): Promise<void> {
     if (!key) {
-      throw new Error('Stripe key is required')
+      this.client = new Stripe(env.stripeKey, { apiVersion: '2023-08-16' })
+      if (!(this.client instanceof Stripe)) {
+        throw new Error('Stripe key is required')
+      }
+      return
     }
     this.client = new Stripe(key, { apiVersion: '2023-08-16' })
   },
 
-  getInstance (): Stripe {
+  async getInstance (): Promise<Stripe> {
     if (!this.client) {
-      throw new Error('There is instance of Stripe')
+      await this.connect(env.stripeKey)
     }
-    return this.client
+    return this.client as Stripe
+  },
+
+  disconnect (): void {
+    this.client = null
   }
 }

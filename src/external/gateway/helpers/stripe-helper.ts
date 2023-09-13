ef56cf8e4dcdc9dc8e6d17987772f885d7334a40
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 17:24:42 -0300

    feat: ensure StripeHelper return Stripe instance if getInstance is a success

--- src/external/gateway/helpers/stripe-helper.ts
+++ src/external/gateway/helpers/stripe-helper.ts
@@ -8,5 +8,12 @@ export const StripeHelper = {
       throw new Error('Stripe key is required')
     }
     this.client = new Stripe(key, { apiVersion: '2023-08-16' })
+  },
+
+  getInstance (): Stripe {
+    if (!this.client) {
+      throw new Error('There is instance of Stripe')
+    }
+    return this.client
   }
 }

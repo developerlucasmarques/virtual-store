50ae97c52b3ba922e8e8e3ec2972b865aef21fef
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 17:40:56 -0300

    test: ensure StripeHelper reconnect if key not provided in connection

--- src/external/gateway/helpers/stripe-helper.test.ts
+++ src/external/gateway/helpers/stripe-helper.test.ts
@@ -3,19 +3,23 @@ import { StripeHelper as sut } from './stripe-helper'
 import env from '@/main/config/env'
 
 describe('Stripe Helper', () => {
+  beforeEach(() => {
+    sut.disconnect()
+  })
+
   it('Should connect with Stripe', async () => {
     await sut.connect(env.stripeKey)
     expect(sut.client).toBeInstanceOf(Stripe)
   })
 
-  it('Should throw if key not provided in connection', async () => {
-    const promise = sut.connect('')
-    await expect(promise).rejects.toThrow()
+  it('Should reconnect if key not provided in connection', async () => {
+    await sut.connect('')
+    expect(sut.client).toBeInstanceOf(Stripe)
   })
 
   it('Should return Stripe instance if getInstance is a success', async () => {
     await sut.connect(env.stripeKey)
-    const instance = sut.getInstance()
+    const instance = await sut.getInstance()
     expect(instance).toBeInstanceOf(Stripe)
   })
 })

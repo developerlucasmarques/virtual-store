8b71285386cc6056f3171b3da37ff814e87b9ca8
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 18:07:53 -0300

    test: ensure StripeHelper disconnect

--- src/external/gateway/helpers/stripe-helper.test.ts
+++ src/external/gateway/helpers/stripe-helper.test.ts
@@ -1,6 +1,8 @@
+import env from '@/main/config/env'
 import Stripe from 'stripe'
 import { StripeHelper as sut } from './stripe-helper'
-import env from '@/main/config/env'
+
+jest.mock('stripe')
 
 describe('Stripe Helper', () => {
   beforeEach(() => {
@@ -12,6 +14,11 @@ describe('Stripe Helper', () => {
     expect(sut.client).toBeInstanceOf(Stripe)
   })
 
+  it('Should call Stripe', async () => {
+    await sut.connect(env.stripeKey)
+    expect(Stripe).toBeCalled()
+  })
+
   it('Should reconnect if key not provided in connection', async () => {
     await sut.connect('')
     expect(sut.client).toBeInstanceOf(Stripe)
@@ -21,5 +28,11 @@ describe('Stripe Helper', () => {
     await sut.connect(env.stripeKey)
     const instance = await sut.getInstance()
     expect(instance).toBeInstanceOf(Stripe)
+  })
+
+  it('Should disconnect', async () => {
+    await sut.connect('')
+    sut.disconnect()
+    expect(sut.client).toBeNull()
   })
 })

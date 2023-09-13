0cc22a888afe25057499868af973a888e4c5ee74
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 15:45:47 -0300

    feat: ensure CheckoutController return 500 if Checkout returns CheckoutFailureError

--- src/presentation/controllers/checkout/checkout-controller.ts
+++ src/presentation/controllers/checkout/checkout-controller.ts
@@ -1,7 +1,7 @@
 import type { Checkout } from '@/domain/usecases-contracts'
-import { EmptyCartError } from '@/domain/usecases-contracts/errors'
+import { CheckoutFailureError, EmptyCartError } from '@/domain/usecases-contracts/errors'
 import type { Controller } from '@/presentation/contracts'
-import { badRequest, notFound } from '@/presentation/helpers/http/http-helpers'
+import { badRequest, notFound, serverError } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class CheckoutController implements Controller {
@@ -12,6 +12,9 @@ export class CheckoutController implements Controller {
     if (checkoutResult.isLeft()) {
       if (checkoutResult.value instanceof EmptyCartError) {
         return badRequest(checkoutResult.value)
+      }
+      if (checkoutResult.value instanceof CheckoutFailureError) {
+        return serverError(checkoutResult.value)
       }
       return notFound(checkoutResult.value)
     }

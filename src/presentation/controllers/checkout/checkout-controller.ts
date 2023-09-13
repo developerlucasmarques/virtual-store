ecf8b37bc99bcf69f85d69078496a6a624610ab6
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 15:39:39 -0300

    feat: ensure CheckoutController return 404 if Checkout returns ProductNotAvailableError

--- src/presentation/controllers/checkout/checkout-controller.ts
+++ src/presentation/controllers/checkout/checkout-controller.ts
@@ -1,12 +1,16 @@
 import type { Checkout } from '@/domain/usecases-contracts'
 import type { Controller } from '@/presentation/contracts'
+import { notFound } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class CheckoutController implements Controller {
   constructor (private readonly checkout: Checkout) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.checkout.perform(httpRequest.headers.userId)
+    const checkoutResult = await this.checkout.perform(httpRequest.headers.userId)
+    if (checkoutResult.isLeft()) {
+      return notFound(checkoutResult.value)
+    }
     return { statusCode: 0, body: '' }
   }
 }

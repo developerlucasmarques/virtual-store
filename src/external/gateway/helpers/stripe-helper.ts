25a86606ffb94c21e7babae156cf85fd8d483a09
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 17:06:44 -0300

    feat: ensure StripeHelper throw if key not provided in connection

--- src/external/gateway/helpers/stripe-helper.ts
+++ src/external/gateway/helpers/stripe-helper.ts
@@ -2,14 +2,11 @@ import Stripe from 'stripe'
 
 export const StripeHelper = {
   client: null as unknown as Stripe | null,
-  key: null as unknown as string,
 
   async connect (key: string | undefined): Promise<void> {
     if (!key) {
-      this.key = 'any_key'
-    } else {
-      this.key = key
+      throw new Error('Stripe key is required')
     }
-    this.client = new Stripe(this.key, { apiVersion: '2023-08-16' })
+    this.client = new Stripe(key, { apiVersion: '2023-08-16' })
   }
 }

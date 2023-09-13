349d908d68d4e16d2496775b0d3c8e76d190465e
mlucasdev <lucasmsantos819@gmail.com>
2023-09-13 15:42:32 -0300

    feat: ensure CheckoutController 400 if Checkout returns EmptyCartError

--- src/presentation/controllers/checkout/checkout-controller.ts
+++ src/presentation/controllers/checkout/checkout-controller.ts
@@ -1,6 +1,7 @@
 import type { Checkout } from '@/domain/usecases-contracts'
+import { EmptyCartError } from '@/domain/usecases-contracts/errors'
 import type { Controller } from '@/presentation/contracts'
-import { notFound } from '@/presentation/helpers/http/http-helpers'
+import { badRequest, notFound } from '@/presentation/helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '@/presentation/http-types/http'
 
 export class CheckoutController implements Controller {
@@ -9,6 +10,9 @@ export class CheckoutController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const checkoutResult = await this.checkout.perform(httpRequest.headers.userId)
     if (checkoutResult.isLeft()) {
+      if (checkoutResult.value instanceof EmptyCartError) {
+        return badRequest(checkoutResult.value)
+      }
       return notFound(checkoutResult.value)
     }
     return { statusCode: 0, body: '' }

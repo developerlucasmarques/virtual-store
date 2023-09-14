307f8452bc325fc43e1091e6b0d40ad88241bcb5
mlucasdev <lucasmsantos819@gmail.com>
2023-09-14 13:02:17 -0300

    feat: add UserMongoRepo in CheckoutUseCase Factory

--- src/main/factories/usecases/checkout/checkout-usecase-factory.ts
+++ src/main/factories/usecases/checkout/checkout-usecase-factory.ts
@@ -2,8 +2,10 @@ import type { Checkout } from '@/domain/usecases-contracts'
 import { StripeAdapter } from '@/external/gateway/stripe/stripe-adapter'
 import { CheckoutUseCase } from '@/interactions/usecases/checkout'
 import { makeLoadCartUseCase } from '../cart'
+import { UserMongoRepo } from '@/external/db/mongo-db/user/user-mongo-repo'
 
 export const makeCheckoutUseCase = (): Checkout => {
+  const userMongoRepo = new UserMongoRepo()
   const stripeAdapter = new StripeAdapter()
-  return new CheckoutUseCase(makeLoadCartUseCase(), stripeAdapter)
+  return new CheckoutUseCase(makeLoadCartUseCase(), userMongoRepo, stripeAdapter)
 }

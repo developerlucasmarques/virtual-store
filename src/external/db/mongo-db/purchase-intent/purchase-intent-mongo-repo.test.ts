d3c378a76451c713ddd4e06fdd86a5a1a4766755
mlucasdev <lucasmsantos819@gmail.com>
2023-09-16 13:18:14 -0300

    test: ensure PurchaseIntentMongoRepo create a PurchaseIntent if add on success

---
+++ src/external/db/mongo-db/purchase-intent/purchase-intent-mongo-repo.test.ts
@@ -0,0 +1,51 @@
+import { ObjectId, type Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { type PurchaseIntentModel } from '@/domain/models'
+import MockDate from 'mockdate'
+import { PurchaseIntentMongoRepo } from './purchase-intent-mongo-repo'
+
+let purchaseIntentCollection: Collection
+
+const idString = new ObjectId().toHexString()
+const objectId = new ObjectId(idString)
+
+const makeFakePurchaseIntentModel = (): PurchaseIntentModel => ({
+  id: idString,
+  userId: 'any_user_id',
+  gatewayCustomerId: 'any_gateway_customer_id',
+  createdAt: new Date(),
+  updateDat: new Date(),
+  products: [{
+    id: 'any_product_id',
+    name: 'any name',
+    amount: 10.90,
+    quantity: 2
+  }]
+})
+
+describe('PurchaseIntentMongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    MockDate.set(new Date())
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    MockDate.reset()
+  })
+
+  beforeEach(async () => {
+    purchaseIntentCollection = await MongoHelper.getCollection('purchase-intent')
+    await purchaseIntentCollection.deleteMany({})
+  })
+
+  it('Should create a PurchaseIntent if add on success', async () => {
+    const sut = new PurchaseIntentMongoRepo()
+    await sut.add(makeFakePurchaseIntentModel())
+    const purchaseIntent = await purchaseIntentCollection.findOne({ _id: objectId })
+    const purchaseIntentWithObejctId = MongoHelper.convertCollectionIdStringToObjectId(
+      makeFakePurchaseIntentModel()
+    )
+    expect(purchaseIntent).toEqual(purchaseIntentWithObejctId)
+  })
+})

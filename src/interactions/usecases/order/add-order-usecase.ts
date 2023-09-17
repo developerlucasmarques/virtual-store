2f27b4b2f26cc02ac7344cd9281ea076d604a95b
mlucasdev <lucasmsantos819@gmail.com>
2023-09-17 16:31:51 -0300

    feat: ensure AddOrderUseCase return PurchaseIntentNotFoundError if LoadPurchaseIntentRepo returns null

--- src/interactions/usecases/order/add-order-usecase.ts
+++ src/interactions/usecases/order/add-order-usecase.ts
@@ -1,12 +1,16 @@
 import type { AddOrder, AddOrderData, AddOrderResponse } from '@/domain/usecases-contracts'
+import { PurchaseIntentNotFoundError } from '@/domain/usecases-contracts/errors'
 import type { LoadPurchaseIntentByIdRepo } from '@/interactions/contracts'
-import { right } from '@/shared/either'
+import { left, right } from '@/shared/either'
 
 export class AddOrderUseCase implements AddOrder {
   constructor (private readonly loadPurchaseIntentByIdRepo: LoadPurchaseIntentByIdRepo) {}
 
   async perform (data: AddOrderData): Promise<AddOrderResponse> {
-    await this.loadPurchaseIntentByIdRepo.loadById(data.purchaseIntentId)
+    const purchaseIntent = await this.loadPurchaseIntentByIdRepo.loadById(data.purchaseIntentId)
+    if (!purchaseIntent) {
+      return left(new PurchaseIntentNotFoundError('any_purchase_intent_id'))
+    }
     return right(null)
   }
 }

6969085c57b2b2e8f65f00a8eda3dfd15457880f
mlucasdev <lucasmsantos819@gmail.com>
2023-09-17 16:37:22 -0300

    feat: ensure AddOrderUseCase call IdBuilder only once

--- src/interactions/usecases/order/add-order-usecase.ts
+++ src/interactions/usecases/order/add-order-usecase.ts
@@ -1,16 +1,20 @@
 import type { AddOrder, AddOrderData, AddOrderResponse } from '@/domain/usecases-contracts'
 import { PurchaseIntentNotFoundError } from '@/domain/usecases-contracts/errors'
-import type { LoadPurchaseIntentByIdRepo } from '@/interactions/contracts'
+import type { IdBuilder, LoadPurchaseIntentByIdRepo } from '@/interactions/contracts'
 import { left, right } from '@/shared/either'
 
 export class AddOrderUseCase implements AddOrder {
-  constructor (private readonly loadPurchaseIntentByIdRepo: LoadPurchaseIntentByIdRepo) {}
+  constructor (
+    private readonly loadPurchaseIntentByIdRepo: LoadPurchaseIntentByIdRepo,
+    private readonly idBuilder: IdBuilder
+  ) {}
 
   async perform (data: AddOrderData): Promise<AddOrderResponse> {
     const purchaseIntent = await this.loadPurchaseIntentByIdRepo.loadById(data.purchaseIntentId)
     if (!purchaseIntent) {
       return left(new PurchaseIntentNotFoundError('any_purchase_intent_id'))
     }
+    this.idBuilder.build()
     return right(null)
   }
 }

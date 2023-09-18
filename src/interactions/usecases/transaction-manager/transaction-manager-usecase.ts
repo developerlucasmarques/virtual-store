2955229a09e98282ac68f26159a2bbbb9a24a440
mlucasdev <lucasmsantos819@gmail.com>
2023-09-18 16:17:22 -0300

    feat: ensure TransactionManagerUseCase call LoadUsertByIdRepo with correct user id

--- src/interactions/usecases/transaction-manager/transaction-manager-usecase.ts
+++ src/interactions/usecases/transaction-manager/transaction-manager-usecase.ts
@@ -1,16 +1,20 @@
 import type { TransactionManager, TransactionManagerData, TransactionManagerResponse } from '@/domain/usecases-contracts'
 import { GatewayIncompatibilityError } from '@/domain/usecases-contracts/errors'
-import type { TransactionListenerGateway } from '@/interactions/contracts'
+import type { LoadUserByIdRepo, TransactionListenerGateway } from '@/interactions/contracts'
 import { left, right } from '@/shared/either'
 
 export class TransactionManagerUseCase implements TransactionManager {
-  constructor (private readonly transactionListenerGateway: TransactionListenerGateway) {}
+  constructor (
+    private readonly transactionListenerGateway: TransactionListenerGateway,
+    private readonly loadUserByIdRepo: LoadUserByIdRepo
+  ) {}
 
   async perform (data: TransactionManagerData): Promise<TransactionManagerResponse> {
     const listenerResult = await this.transactionListenerGateway.listener(data)
     if (!listenerResult) {
       return left(new GatewayIncompatibilityError())
     }
+    await this.loadUserByIdRepo.loadById(listenerResult.userId)
     return right(null)
   }
 }

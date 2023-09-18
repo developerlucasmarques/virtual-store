137d1917462f49a46ec8d13f9d6f50f546680ff2
mlucasdev <lucasmsantos819@gmail.com>
2023-09-18 16:17:32 -0300

    test: ensure TransactionManagerUseCase call LoadUsertByIdRepo with correct user id

--- src/interactions/usecases/transaction-manager/transaction-manager-usecase.spec.ts
+++ src/interactions/usecases/transaction-manager/transaction-manager-usecase.spec.ts
@@ -1,13 +1,23 @@
 import type { TransactionManagerData } from '@/domain/usecases-contracts'
-import type { TransactionListenerGateway, TransactionListenerGatewayData, TransactionListenerGatewayResponse } from '@/interactions/contracts'
+import type { LoadUserByIdRepo, TransactionListenerGateway, TransactionListenerGatewayData, TransactionListenerGatewayResponse } from '@/interactions/contracts'
 import { TransactionManagerUseCase } from './transaction-manager-usecase'
 import { GatewayIncompatibilityError } from '@/domain/usecases-contracts/errors'
+import type { UserModel } from '@/domain/models'
 
 const makeFakeTransactionManagerData = (): TransactionManagerData => ({
   payload: {
     field: 'any_value'
   },
   signature: 'any_signature'
+})
+
+const makeFakeUserModel = (): UserModel => ({
+  id: 'any_id',
+  name: 'any name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password',
+  role: 'admin',
+  accessToken: 'any_token'
 })
 
 const makeTransactionListenerGateway = (): TransactionListenerGateway => {
@@ -21,17 +31,29 @@ const makeTransactionListenerGateway = (): TransactionListenerGateway => {
   return new TransactionListenerGatewayStub()
 }
 
+const makeLoadUserByIdRepo = (): LoadUserByIdRepo => {
+  class LoadUserByIdRepoStub implements LoadUserByIdRepo {
+    async loadById (id: string): Promise<null | UserModel> {
+      return await Promise.resolve(makeFakeUserModel())
+    }
+  }
+  return new LoadUserByIdRepoStub()
+}
+
 type SutTypes = {
   sut: TransactionManagerUseCase
   transactionListenerGatewayStub: TransactionListenerGateway
+  loadUserByIdRepoStub: LoadUserByIdRepo
 }
 
 const makeSut = (): SutTypes => {
   const transactionListenerGatewayStub = makeTransactionListenerGateway()
-  const sut = new TransactionManagerUseCase(transactionListenerGatewayStub)
+  const loadUserByIdRepoStub = makeLoadUserByIdRepo()
+  const sut = new TransactionManagerUseCase(transactionListenerGatewayStub, loadUserByIdRepoStub)
   return {
     sut,
-    transactionListenerGatewayStub
+    transactionListenerGatewayStub,
+    loadUserByIdRepoStub
   }
 }
 
@@ -66,5 +88,12 @@ describe('TransactionManager UseCase', () => {
     )
     const promise = sut.perform(makeFakeTransactionManagerData())
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call LoadUsertByIdRepo with correct user id', async () => {
+    const { sut, loadUserByIdRepoStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadUserByIdRepoStub, 'loadById')
+    await sut.perform(makeFakeTransactionManagerData())
+    expect(loadByIdSpy).toBeCalledWith('any_user_id')
   })
 })

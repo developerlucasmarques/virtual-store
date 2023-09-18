9daea447c6deee45e3413e35fc5c8baeb47ed3a7
mlucasdev <lucasmsantos819@gmail.com>
2023-09-18 18:36:02 -0300

    test: ensure TransactionManagerController call TransactionManager with correct values

--- src/presentation/controllers/transaction-manager/transaction-manager-controller.spec.ts
+++ src/presentation/controllers/transaction-manager/transaction-manager-controller.spec.ts
@@ -1,15 +1,39 @@
 import { PayloadNotInformedError, SignatureNotInformedError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http/http-helpers'
 import { TransactionManagerController } from './transaction-manager-controller'
+import type { HttpRequest } from '@/presentation/http-types/http'
+import { type TransactionManager, type TransactionManagerData, type TransactionManagerResponse } from '@/domain/usecases-contracts'
+import { right } from '@/shared/either'
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: {
+    signature: 'any_signature'
+  },
+  body: {
+    payload: 'any_payload'
+  }
+})
+
+const makeTransactionManager = (): TransactionManager => {
+  class TransactionManagerStub implements TransactionManager {
+    async perform (data: TransactionManagerData): Promise<TransactionManagerResponse> {
+      return await Promise.resolve(right(null))
+    }
+  }
+  return new TransactionManagerStub()
+}
 
 type SutTypes = {
   sut: TransactionManagerController
+  transactionManagerStub: TransactionManager
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new TransactionManagerController()
+  const transactionManagerStub = makeTransactionManager()
+  const sut = new TransactionManagerController(transactionManagerStub)
   return {
-    sut
+    sut,
+    transactionManagerStub
   }
 }
 
@@ -26,5 +50,15 @@ describe('TransactionManager Controller', () => {
       headers: { signature: 'any_signature' }
     })
     expect(httpResponse).toEqual(badRequest(new PayloadNotInformedError()))
+  })
+
+  it('Should call TransactionManager with correct values', async () => {
+    const { sut, transactionManagerStub } = makeSut()
+    const performSpy = jest.spyOn(transactionManagerStub, 'perform')
+    await sut.handle(makeFakeRequest())
+    expect(performSpy).toHaveBeenCalledWith({
+      signature: 'any_signature',
+      payload: 'any_payload'
+    })
   })
 })

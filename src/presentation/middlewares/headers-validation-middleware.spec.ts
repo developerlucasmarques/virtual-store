2c276dfac87c3200c1b612e4492d559a8d7f6870
mlucasdev <lucasmsantos819@gmail.com>
2023-09-20 16:47:22 -0300

    test: ensure HeadersValidationMiddleware return 400 if Validation fails

--- src/presentation/middlewares/headers-validation-middleware.spec.ts
+++ src/presentation/middlewares/headers-validation-middleware.spec.ts
@@ -1,7 +1,8 @@
-import { right, type Either } from '@/shared/either'
+import { right, type Either, left } from '@/shared/either'
 import type { Validation } from '../contracts'
 import type { HttpRequest } from '../http-types/http'
 import { HeadersValidationMiddleware } from './headers-validation-middleware'
+import { badRequest } from '../helpers/http/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   headers: { anyField: 'any_value', anotherField: 'another_value' }
@@ -36,5 +37,14 @@ describe('HeadersValidation Middleware', () => {
     const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(makeFakeRequest())
     expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().headers)
+  })
+
+  it('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(
+      left(new Error('any_message'))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error('any_message')))
   })
 })

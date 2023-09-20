652882cd8d4252d1f459e199707fa1de507a1174
mlucasdev <lucasmsantos819@gmail.com>
2023-09-20 17:03:17 -0300

    feat: create stripeAdaptMiddleware

---
+++ src/main/adapters/stripe/stripe-middleware-adapter.ts
@@ -0,0 +1,23 @@
+import type { NextFunction, Request, Response } from 'express'
+import type { Middleware } from '@/presentation/contracts'
+import type { HttpRequest } from '@/presentation/http-types/http'
+
+export const stripeAdaptMiddleware = (middleare: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpResponse = await middleare.handle(httpRequest)
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      Object.assign(req.headers, { signature: req.headers['stripe-signature'] })
+      Object.assign(req.body.payload, req.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        name: httpResponse.body.name,
+        error: httpResponse.body.message,
+        statusCode: httpResponse.statusCode
+      })
+    }
+  }
+}
--- src/main/config/custom-namespaces.d.ts
+++ src/main/config/custom-namespaces.d.ts
@@ -2,6 +2,7 @@ declare namespace Express {
   interface Request {
     headers: {
       userId?: string
+      signature?: string
     }
   }
 }

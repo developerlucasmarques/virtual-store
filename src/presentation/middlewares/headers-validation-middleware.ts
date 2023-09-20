ee698acabb7309e47533f0147ca6c83467afffe9
mlucasdev <lucasmsantos819@gmail.com>
2023-09-20 16:47:13 -0300

    feat: ensure HeadersValidationMiddleware return 400 if Validation fails

--- src/presentation/middlewares/headers-validation-middleware.ts
+++ src/presentation/middlewares/headers-validation-middleware.ts
@@ -1,11 +1,15 @@
 import type { Middleware, Validation } from '../contracts'
+import { badRequest } from '../helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 
 export class HeadersValidationMiddleware implements Middleware {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.headers)
+    const validation = this.validation.validate(httpRequest.headers)
+    if (validation.isLeft()) {
+      return badRequest(validation.value)
+    }
     return { statusCode: 0, body: '' }
   }
 }

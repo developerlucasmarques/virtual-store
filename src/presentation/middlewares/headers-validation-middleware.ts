eabc188344ed67b49a06a791741869bbbf5a83a1
mlucasdev <lucasmsantos819@gmail.com>
2023-09-20 16:49:06 -0300

    feat: ensure HeadersValidationMiddleware return 500 if Validation throws

--- src/presentation/middlewares/headers-validation-middleware.ts
+++ src/presentation/middlewares/headers-validation-middleware.ts
@@ -1,15 +1,19 @@
 import type { Middleware, Validation } from '../contracts'
-import { badRequest } from '../helpers/http/http-helpers'
+import { badRequest, serverError } from '../helpers/http/http-helpers'
 import type { HttpRequest, HttpResponse } from '../http-types/http'
 
 export class HeadersValidationMiddleware implements Middleware {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validation = this.validation.validate(httpRequest.headers)
-    if (validation.isLeft()) {
-      return badRequest(validation.value)
+    try {
+      const validation = this.validation.validate(httpRequest.headers)
+      if (validation.isLeft()) {
+        return badRequest(validation.value)
+      }
+      return { statusCode: 0, body: '' }
+    } catch (error: any) {
+      return serverError(error)
     }
-    return { statusCode: 0, body: '' }
   }
 }

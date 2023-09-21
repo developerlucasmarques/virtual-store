9e013fb2b76f7a1453ec241e1edd8b8a1cc6a91a
mlucasdev <lucasmsantos819@gmail.com>
2023-09-21 18:58:15 -0300

    refactor: remove unused files

--- src/presentation/errors/payload-not-informed-error.ts
+++
@@ -1,6 +0,0 @@
-export class PayloadNotInformedError extends Error {
-  constructor () {
-    super('Payload not informed')
-    this.name = 'PayloadNotInformedError'
-  }
-}
--- src/presentation/errors/signature-not-informed-error.ts
+++
@@ -1,6 +0,0 @@
-export class SignatureNotInformedError extends Error {
-  constructor () {
-    super('Signature not informed')
-    this.name = 'SignatureNotInformedError'
-  }
-}

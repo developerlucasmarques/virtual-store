f216612714fb8fa1348fc2934e056413650f02ae
mlucasdev <lucasmsantos819@gmail.com>
2023-09-20 21:05:48 -0300

    feat: ensure PurchaseIntentMongoRepo load purchase intent if loadById is a success

--- src/external/db/mongo-db/purchase-intent/purchase-intent-mongo-repo.ts
+++ src/external/db/mongo-db/purchase-intent/purchase-intent-mongo-repo.ts
@@ -1,11 +1,18 @@
 import type { PurchaseIntentModel } from '@/domain/models'
-import type { AddPurchaseIntentRepo } from '@/interactions/contracts'
+import type { AddPurchaseIntentRepo, LoadPurchaseIntentByIdRepo } from '@/interactions/contracts'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class PurchaseIntentMongoRepo implements AddPurchaseIntentRepo {
+export class PurchaseIntentMongoRepo implements AddPurchaseIntentRepo, LoadPurchaseIntentByIdRepo {
   async add (data: PurchaseIntentModel): Promise<void> {
     const purchaseIntent = MongoHelper.convertCollectionIdStringToObjectId(data)
     const purchaseIntentCollection = await MongoHelper.getCollection('purchase-intent')
     await purchaseIntentCollection.insertOne(purchaseIntent)
+  }
+
+  async loadById (id: string): Promise<null | PurchaseIntentModel > {
+    const purchaseIntentCollection = await MongoHelper.getCollection('purchase-intent')
+    const objectId = MongoHelper.transformIdInObjectId(id)
+    const purchaseIntent = await purchaseIntentCollection.findOne({ _id: objectId })
+    return MongoHelper.convertCollectionIdObjectIdToString(purchaseIntent)
   }
 }

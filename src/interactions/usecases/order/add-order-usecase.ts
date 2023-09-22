fd3858feb4f3433dba00c4cdf2ccb1ddc0876563
mlucasdev <lucasmsantos819@gmail.com>
2023-09-22 11:22:57 -0300

    fix: inform PurchaseIntentNotFoundError the purchase intent id

--- src/interactions/usecases/order/add-order-usecase.ts
+++ src/interactions/usecases/order/add-order-usecase.ts
@@ -14,7 +14,7 @@ export class AddOrderUseCase implements AddOrder {
   async perform (data: AddOrderData): Promise<AddOrderResponse> {
     const purchaseIntent = await this.loadPurchaseIntentByIdRepo.loadById(data.purchaseIntentId)
     if (!purchaseIntent) {
-      return left(new PurchaseIntentNotFoundError('any_purchase_intent_id'))
+      return left(new PurchaseIntentNotFoundError(data.purchaseIntentId))
     }
     const { id } = this.idBuilder.build()
     await this.addOrderRepo.add({ id, userId: data.userId, products: purchaseIntent.products })

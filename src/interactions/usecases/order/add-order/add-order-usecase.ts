b2f3b77d50194db71dfe7d1d6046e469561f4bd6
mlucasdev <lucasmsantos819@gmail.com>
2023-09-25 17:17:10 -0300

    feat: ensure AddOrderUseCase call GenerateOrderCode with correct user id

--- src/interactions/usecases/order/add-order/add-order-usecase.ts
+++ src/interactions/usecases/order/add-order/add-order-usecase.ts
@@ -1,5 +1,5 @@
 import type { PaymentStatusOfOrderModel, StatusOfOrderModel } from '@/domain/models'
-import type { AddOrder, AddOrderData } from '@/domain/usecases-contracts'
+import type { AddOrder, AddOrderData, GenerateOrderCode } from '@/domain/usecases-contracts'
 import type { AddOrderRepo, IdBuilder } from '@/interactions/contracts'
 import { right, type Either } from '@/shared/either'
 
@@ -9,15 +9,17 @@ export class AddOrderUseCase implements AddOrder {
     private readonly idBuilder: IdBuilder,
     private readonly status: StatusOfOrderModel,
     private readonly paymentStatus: PaymentStatusOfOrderModel,
+    private readonly generateOrderCode: GenerateOrderCode,
     private readonly addOrderRepo: AddOrderRepo
   ) {}
 
   async perform (data: AddOrderData): Promise<Either<Error, null>> {
-    const { orderCode, products, userId } = data
+    const { products, userId } = data
     const { id } = this.idBuilder.build()
     const createdAt = new Date(); const updatedAt = createdAt
     const paymentStatus = this.paymentStatus
     const status = this.status
+    const { code: orderCode } = await this.generateOrderCode.perform(data.userId)
     await this.addOrderRepo.add(
       { id, userId, orderCode, products, createdAt, updatedAt, paymentStatus, status }
     )

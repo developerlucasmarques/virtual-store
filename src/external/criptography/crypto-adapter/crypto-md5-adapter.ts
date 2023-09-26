e8ed2af58550f67042fcffb9b2606f6a398c03d2
mlucasdev <lucasmsantos819@gmail.com>
2023-09-26 15:43:45 -0300

    feat: ensure CryptoMd5Adapter return this same hash as crypto returns

--- src/external/criptography/crypto-adapter/crypto-md5-adapter.ts
+++ src/external/criptography/crypto-adapter/crypto-md5-adapter.ts
@@ -3,7 +3,7 @@ import * as crypto from 'crypto'
 
 export class CryptoMd5Adapter implements Encrypter {
   async encrypt (data: EncrypterData): Promise<Token> {
-    crypto.createHash('md5').update(data.value).digest('hex')
-    return await Promise.resolve({ token: '' })
+    const hash = crypto.createHash('md5').update(data.value).digest('hex')
+    return await Promise.resolve({ token: hash })
   }
 }

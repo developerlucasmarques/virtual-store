68d705938f09f9c43ada418a8da9809eb688b668
mlucasdev <lucasmsantos819@gmail.com>
2023-09-26 15:33:47 -0300

    test: ensure CryptoMd5Adapter throw if sign throws

--- src/external/criptography/crypto-adapter/crypto-md5-adapter.spec.ts
+++ src/external/criptography/crypto-adapter/crypto-md5-adapter.spec.ts
@@ -15,21 +15,30 @@ const makeSut = (): CryptoMd5Adapter => {
 describe('Crypto Adapter', () => {
   it('Should call crypto createHash with data using "MD5"', async () => {
     const sut = makeSut()
-    await sut.encrypt({ value: 'testData' })
+    await sut.encrypt({ value: 'any-value' })
     expect(crypto.createHash).toHaveBeenCalledWith('md5')
   })
 
   it('Should call crypto update with correct value', async () => {
     const sut = makeSut()
-    await sut.encrypt({ value: 'testData' })
+    await sut.encrypt({ value: 'any-value' })
     const update = crypto.createHash('md5').update
-    expect(update).toHaveBeenCalledWith('testData')
+    expect(update).toHaveBeenCalledWith('any-value')
   })
 
   it('Should call crypto update digest with data using "hex"', async () => {
     const sut = makeSut()
-    await sut.encrypt({ value: 'testData' })
-    const digest = crypto.createHash('md5').update('testData').digest
+    await sut.encrypt({ value: 'any-value' })
+    const digest = crypto.createHash('md5').update('any-value').digest
     expect(digest).toHaveBeenCalledWith('hex')
+  })
+
+  it('Should throw if sign throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(crypto, 'createHash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt({ value: 'any-value' })
+    await expect(promise).rejects.toThrow()
   })
 })

3de840a1a499a54904390a4631f1b6904e07fb00
mlucasdev <lucasmsantos819@gmail.com>
2023-09-26 15:11:29 -0300

    test: ensure CryptoMd5Adapter call crypto createHash with data using MD5

---
+++ src/external/criptography/crypto-adapter/crypto-md5-adapter.spec.ts
@@ -0,0 +1,20 @@
+import * as crypto from 'crypto'
+import { CryptoMd5Adapter } from './crypto-md5-adapter'
+
+jest.mock('crypto', () => ({
+  createHash: jest.fn().mockReturnThis(),
+  update: jest.fn().mockReturnThis(),
+  digest: jest.fn()
+}))
+
+const makeSut = (): CryptoMd5Adapter => {
+  return new CryptoMd5Adapter()
+}
+
+describe('Crypto Adapter', () => {
+  it('Should call crypto createHash with data using MD5', async () => {
+    const sut = makeSut()
+    await sut.encrypt({ value: 'testData' })
+    expect(crypto.createHash).toHaveBeenCalledWith('md5')
+  })
+})

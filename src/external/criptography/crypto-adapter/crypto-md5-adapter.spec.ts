ef59dc123837785a7be0aba94357d2e32d55d34d
mlucasdev <lucasmsantos819@gmail.com>
2023-09-26 15:43:50 -0300

    test: ensure CryptoMd5Adapter return this same hash as crypto returns

--- src/external/criptography/crypto-adapter/crypto-md5-adapter.spec.ts
+++ src/external/criptography/crypto-adapter/crypto-md5-adapter.spec.ts
@@ -5,7 +5,7 @@ import { CryptoMd5Adapter } from './crypto-md5-adapter'
 jest.mock('crypto', () => ({
   createHash: jest.fn().mockReturnThis(),
   update: jest.fn().mockReturnThis(),
-  digest: jest.fn()
+  digest: jest.fn().mockReturnValue('any_hash')
 }))
 
 const makeSut = (): CryptoMd5Adapter => {
@@ -15,21 +15,21 @@ const makeSut = (): CryptoMd5Adapter => {
 describe('Crypto Adapter', () => {
   it('Should call crypto createHash with data using "MD5"', async () => {
     const sut = makeSut()
-    await sut.encrypt({ value: 'any-value' })
+    await sut.encrypt({ value: 'any_value' })
     expect(crypto.createHash).toHaveBeenCalledWith('md5')
   })
 
   it('Should call crypto update with correct value', async () => {
     const sut = makeSut()
-    await sut.encrypt({ value: 'any-value' })
+    await sut.encrypt({ value: 'any_value' })
     const update = crypto.createHash('md5').update
-    expect(update).toHaveBeenCalledWith('any-value')
+    expect(update).toHaveBeenCalledWith('any_value')
   })
 
   it('Should call crypto update digest with data using "hex"', async () => {
     const sut = makeSut()
-    await sut.encrypt({ value: 'any-value' })
-    const digest = crypto.createHash('md5').update('any-value').digest
+    await sut.encrypt({ value: 'any_value' })
+    const digest = crypto.createHash('md5').update('any_value').digest
     expect(digest).toHaveBeenCalledWith('hex')
   })
 
@@ -38,7 +38,13 @@ describe('Crypto Adapter', () => {
     jest.spyOn(crypto, 'createHash').mockImplementationOnce(() => {
       throw new Error()
     })
-    const promise = sut.encrypt({ value: 'any-value' })
+    const promise = sut.encrypt({ value: 'any_value' })
     await expect(promise).rejects.toThrow()
+  })
+
+  it('Should return this same hash as crypto returns', async () => {
+    const sut = makeSut()
+    const result = await sut.encrypt({ value: 'any_value' })
+    expect(result).toEqual({ token: 'any_hash' })
   })
 })

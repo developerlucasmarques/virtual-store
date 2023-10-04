19262c957a63043983cffb973abb2edb6000a720
mlucasdev <lucasmsantos819@gmail.com>
2023-10-04 19:04:06 -0300

    refactor: rename OrderFormatEmail to FormatEmailApplication

--- src/interactions/application/mail/format-email-application.spec.ts
+++ src/interactions/application/mail/format-email-application.spec.ts
@@ -1,49 +1,51 @@
 import type { EmailTemplate, EmailTemplateResponse } from '@/domain/application-contracts'
-import { OrderFormatEmail } from './order-format-email'
+import { FormatEmailApplication } from './format-email-application'
 
-type OrderFormatEmailData = {
+type FormatEmailData = {
   userName: string
   orderCode: string
-  another: string
+  anotherField: string
 }
 
-const makeFakeOrderFormatEmailData = (): OrderFormatEmailData => ({
+const makeFakeFormatEmailData = (): FormatEmailData => ({
   userName: 'any_user_name',
   orderCode: 'any_order_code',
-  another: 'any'
+  anotherField: 'another_value'
 })
 
 const makeEmailTemplate = (): EmailTemplate => {
   class EmailTemplateStub implements EmailTemplate {
     handle (): EmailTemplateResponse {
-      return { html: 'Hello {{userName}} {{orderCode}} {{another}}' }
+      return { html: 'Name: {{userName}}. Order: {{orderCode}}. Another: {{anotherField}}' }
     }
   }
   return new EmailTemplateStub()
 }
 
 type SutTypes = {
-  sut: OrderFormatEmail<OrderFormatEmailData>
+  sut: FormatEmailApplication<FormatEmailData>
   emailTemplateStub: EmailTemplate
 }
 
 const makeSut = (): SutTypes => {
   const emailTemplateStub = makeEmailTemplate()
-  const sut = new OrderFormatEmail<OrderFormatEmailData>(emailTemplateStub)
+  const sut = new FormatEmailApplication<FormatEmailData>(emailTemplateStub)
   return { sut, emailTemplateStub }
 }
 
-describe('OrderFormatEmail', () => {
+describe('FormatEmail Application', () => {
   it('Should call EmailTemplate', async () => {
     const { sut, emailTemplateStub } = makeSut()
     const handleSpy = jest.spyOn(emailTemplateStub, 'handle')
-    await sut.execute(makeFakeOrderFormatEmailData())
+    await sut.execute(makeFakeFormatEmailData())
     expect(handleSpy).toHaveBeenCalled()
   })
 
   it('Should return format email templete with correct data', async () => {
     const { sut } = makeSut()
-    const result = await sut.execute(makeFakeOrderFormatEmailData())
-    expect(result).toEqual('Hello any_user_name any_order_code any')
+    const result = await sut.execute(makeFakeFormatEmailData())
+    expect(result).toEqual(
+      'Name: any_user_name. Order: any_order_code. Another: another_value'
+    )
   })
 })
--- src/interactions/application/mail/format-email-application.ts
+++ src/interactions/application/mail/format-email-application.ts
@@ -1,6 +1,6 @@
 import type { EmailTemplate, FormatEmail } from '@/domain/application-contracts'
 
-export class OrderFormatEmail<T> implements FormatEmail<T> {
+export class FormatEmailApplication<T> implements FormatEmail<T> {
   constructor (private readonly emailTemplate: EmailTemplate) {}
 
   async execute (data: T): Promise<string> {

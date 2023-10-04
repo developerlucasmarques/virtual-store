f2bf32611789005315210a34ef3c40e7dfde7d2b
mlucasdev <lucasmsantos819@gmail.com>
2023-10-04 18:59:28 -0300

    feat: ensure OrderFormatEmail return format email templete with correct data

--- src/interactions/application/mail/order-format-email.ts
+++ src/interactions/application/mail/order-format-email.ts
@@ -4,7 +4,11 @@ export class OrderFormatEmail<T> implements FormatEmail<T> {
   constructor (private readonly emailTemplate: EmailTemplate) {}
 
   async execute (data: T): Promise<string> {
-    this.emailTemplate.handle()
-    return ''
+    const { html } = this.emailTemplate.handle()
+    let template = html
+    for (const key in data) {
+      template = template.replace(`{{${key}}}`, String(data[key]))
+    }
+    return template
   }
 }

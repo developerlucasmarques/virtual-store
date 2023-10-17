bf7efab81f566a97b5f086744ea912eaae1ebc59
mlucasdev <lucasmsantos819@gmail.com>
2023-10-17 18:59:15 -0300

    test: ensure NodemailerAdapter call nodemailer send mail with correct values

---
+++ src/external/mail/nodemailer/nodemailer-adapter.spec.ts
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer'
+import { NodemailerAdapter } from './nodemailer-adapter'
+import type { EmailSenderProviderData } from '@/interactions/contracts'
+
+const makeFakeEmailSenderProviderData = (): EmailSenderProviderData => ({
+  recipientEmail: 'any_recipient_email@mail.com',
+  recipientName: 'any_recipient_name',
+  subject: 'any_subject',
+  html: 'any_html'
+})
+
+const makeSut = (): NodemailerAdapter => {
+  return new NodemailerAdapter()
+}
+
+jest.mock('nodemailer', () => ({
+  createTransport: jest.fn().mockReturnThis(),
+  sendMail: jest.fn()
+}))
+
+describe('Nodemailer Adapter', () => {
+  it('Should call nodemailer send mail with correct values', async () => {
+    const sut = makeSut()
+    await sut.sendEmail(makeFakeEmailSenderProviderData())
+    expect(nodemailer.createTransport().sendMail).toHaveBeenCalledWith({
+      from: 'Virtual Store',
+      to: 'any_recipient_email@mail.com',
+      subject: 'any_subject',
+      html: 'any_html'
+    })
+  })
+})

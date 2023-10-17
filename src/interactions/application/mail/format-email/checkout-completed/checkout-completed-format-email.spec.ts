8f05be8eac25c41c320ec019542d3aacef49fe58
mlucasdev <lucasmsantos819@gmail.com>
2023-10-17 14:17:29 -0300

    refactor: change to CheckoutCompletedFormatEmail implements OrderFormatEmail

--- src/interactions/application/mail/format-email/checkout-completed/checkout-completed-format-email.spec.ts
+++ src/interactions/application/mail/format-email/checkout-completed/checkout-completed-format-email.spec.ts
@@ -1,19 +1,7 @@
-import { type EmailTemplate, type EmailTemplateResponse } from '@/domain/application-contracts'
-import { FormatCheckoutCompletedEmailApplication } from './format-email-checkout-completed'
+import type { EmailTemplate, EmailTemplateResponse, OrderFormatEmailData } from '@/domain/application-contracts'
+import { CheckoutCompletedFormatEmail } from './checkout-completed-format-email'
 
-type FormatEmailProduct = {
-  name: string
-  amount: number
-  quantity: number
-}
-
-type FormatEmailData = {
-  userName: string
-  orderCode: string
-  products: FormatEmailProduct[]
-}
-
-const makeFakeFormatEmailData = (): FormatEmailData => ({
+const makeFakeOrderFormatEmailData = (): OrderFormatEmailData => ({
   userName: 'any_user_name',
   orderCode: 'any_order_code',
   products: [{
@@ -38,13 +26,13 @@ const makeEmailTemplate = (): EmailTemplate => {
 }
 
 type SutTypes = {
-  sut: FormatCheckoutCompletedEmailApplication
+  sut: CheckoutCompletedFormatEmail
   emailTemplateStub: EmailTemplate
 }
 
 const makeSut = (): SutTypes => {
   const emailTemplateStub = makeEmailTemplate()
-  const sut = new FormatCheckoutCompletedEmailApplication(emailTemplateStub)
+  const sut = new CheckoutCompletedFormatEmail(emailTemplateStub)
   return { sut, emailTemplateStub }
 }
 
@@ -52,13 +40,13 @@ describe('FormatCheckoutCompletedEmail Application', () => {
   it('Should call EmailTemplate', async () => {
     const { sut, emailTemplateStub } = makeSut()
     const handleSpy = jest.spyOn(emailTemplateStub, 'handle')
-    sut.execute(makeFakeFormatEmailData())
+    sut.execute(makeFakeOrderFormatEmailData())
     expect(handleSpy).toHaveBeenCalled()
   })
 
   it('Should return EmailTemplate formated', async () => {
     const { sut } = makeSut()
-    const result = sut.execute(makeFakeFormatEmailData())
+    const result = sut.execute(makeFakeOrderFormatEmailData())
     expect(result.html).toEqual(
       'Name: any_user_name. OrderCode: any_order_code. Products: <li>2 any_product 10.90</li><li>1 another_product 12.90</li>'
     )

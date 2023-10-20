e541082ec5b788c0f0ac314efc49162c69a79e76
mlucasdev <lucasmsantos819@gmail.com>
2023-10-20 16:29:38 -0300

    test: ensure TransactioManagerUseCase Factory call SendCheckoutCompletedEmail Factory

--- src/main/factories/usecases/transaction-manager/transaction-manager-usecase-factory.spec.ts
+++ src/main/factories/usecases/transaction-manager/transaction-manager-usecase-factory.spec.ts
@@ -2,6 +2,7 @@ import type { Event, TransactionEventType } from '@/domain/usecases-contracts'
 import { EventManagerUseCase } from '@/interactions/usecases/event-manager'
 import { makeTransactioManagerUseCase } from './transaction-manager-usecase-factory'
 import { makeUpdateOrderUseCase } from '../order'
+import { makeSendCheckoutCompletedEmail } from '../mail/send-email/send-checkout-completed-email-factory'
 
 jest.mock('@/interactions/usecases/event-manager/event-manager-usecase')
 
@@ -12,7 +13,8 @@ describe('TransactioManagerUseCase Factory', () => {
     makeTransactioManagerUseCase()
     const eventConfig: EventConfig = {
       CheckoutCompleted: [
-        makeUpdateOrderUseCase({ status: 'Processing', paymentStatus: 'Payment_Pending' })
+        makeUpdateOrderUseCase({ status: 'Processing', paymentStatus: 'Payment_Pending' }),
+        makeSendCheckoutCompletedEmail()
       ],
       PaymentSuccess: [],
       PaymentFailure: []
